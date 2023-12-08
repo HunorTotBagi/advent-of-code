07774a82ccd41bb1a2561bc0b2a9bbb5eef1cce4
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-08 16:49:44 +0100

    task03: init

---
+++ aoc/day03-gear-ratios/task03.cs
@@ -0,0 +1,194 @@
+﻿namespace test.day03_gear_ratios
+{
+    public class Schematic
+    {
+        public List<string> ExtendSchematic(string filePath)
+        {
+            List<string> matrix = ReadFileToList(filePath);
+
+            int rows = matrix.Count;
+            int cols = matrix[0].Length;
+
+            List<string> extendedMatrix = new List<string>(rows + 2);
+
+            extendedMatrix.Add(new string('.', cols + 2));
+
+            for (int i = 0; i < rows; i++)
+            {
+                extendedMatrix.Add("." + matrix[i] + ".");
+            }
+
+            extendedMatrix.Add(new string('.', cols + 2));
+
+            return extendedMatrix;
+        }
+
+        public int SummUpAllNumbersInSchematic(string filePath)
+        {
+            List<string> documents2 = ReadFileToList(filePath);
+
+            return SumNumberGroupsInGrid(documents2);
+        }
+
+        static int SumNumberGroupsInGrid(List<string> grid)
+        {
+            int sum = 0;
+
+            foreach (var row in grid)
+            {
+                string[] numberGroups = row.Split(new[] { '*', '.', '+', '$', '#' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var group in numberGroups)
+                {
+                    if (int.TryParse(group, out int groupValue))
+                    {
+                        sum += groupValue;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        //public bool CheckLeft(List<string> extendedSchema, int i)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool IsTheNumberFirst(List<string> extendedSchema, int i, int j)
+        {
+            if (extendedSchema[i - 1][j] == '.' &&
+                extendedSchema[i - 1][j - 1] == '.' &&
+                extendedSchema[i][j - 1] == '.' &&
+                extendedSchema[i + 1][j - 1] == '.' &&
+                extendedSchema[i + 1][j] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTheNumberMiddle(List<string> extendedSchema, int i, int j)
+        {
+            if (extendedSchema[i - 1][j] == '.' && extendedSchema[i + 1][j] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTheNumberLast(List<string> extendedSchema, int i, int j)
+        {
+            if (extendedSchema[i - 1][j] == '.' &&
+                extendedSchema[i - 1][j + 1] == '.' &&
+                extendedSchema[i][j + 1] == '.' &&
+                extendedSchema[i + 1][j + 1] == '.' &&
+                extendedSchema[i + 1][j] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int IsAPartNumber(List<string> extendedSchema)
+        {
+            int rows = extendedSchema.Count;
+            int columns = extendedSchema[0].Length;
+
+            for (int i = 1; i < rows - 1; i++)
+            {
+                for (int j = 1; j < columns - 1; j++)
+                {
+                    if (IsTheNumberFirst(extendedSchema, i, j))
+                    {
+                        int k = 0;
+                        while (char.IsDigit(extendedSchema[i][j + 1]))
+                        {
+                            if (IsTheNumberMiddle(extendedSchema, i, j + k + 1))
+                            {
+                                return 1;
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            }
+            return 1;
+        }
+
+        public List<int> GetLenghtOfAllNumbers(string filePath)
+        {
+            List<string> data = ReadFileToList(filePath);
+            int rows = data.Count;
+            int columns = data[0].Length;
+            int counter = 0;
+            List<int> result = new List<int>();
+
+            for (int i = 1; i < rows - 1; i++)
+            {
+                for (int j = 1; j < columns - 1; j++)
+                {
+                    if (char.IsDigit(data[i][j]))
+                    {
+                        counter += 1;
+                    }
+                    else
+                    {
+                        if (counter != 0)
+                        {
+                            result.Add(counter);
+                        }
+                        counter = 0;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public bool Dimension1(string filePath, int i, int j)
+        {
+            List<string> matrix = ReadFileToList(filePath);
+
+            if (IsTheNumberFirst(matrix, i, j) && IsTheNumberLast(matrix, i, j))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Dimension2(string filePath, int i, int j)
+        {
+            List<string> matrix = ReadFileToList(filePath);
+
+            if (IsTheNumberFirst(matrix, i, j - 1) && IsTheNumberLast(matrix, i, j))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Dimension3(string filePath, int i, int j)
+        {
+            List<string> matrix = ReadFileToList(filePath);
+
+            if (IsTheNumberFirst(matrix, i, j - 2) && IsTheNumberMiddle(matrix, i, j - 1) && IsTheNumberLast(matrix, i, j))
+            {
+                return true;
+            }
+            return false; ;
+        }
+        public List<string> ReadFileToList(string filePath)
+        {
+            List<string> linesList = new List<string>();
+
+            string[] lines = File.ReadAllLines(filePath);
+            linesList.AddRange(lines);
+
+            return linesList;
+        }
+    }
+}
---
+++ test/day03-gear-ratios/task03test.cs
@@ -0,0 +1,148 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace test.day03_gear_ratios
+{
+    public class task03test
+    {
+        Schematic schematic = new Schematic();
+
+        [Fact]
+        public void Should_return_summ_of_all_numbers_in_schema()
+        {
+            // Arrange
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\engineSchematic.txt";
+
+            // Act
+            int result = schematic.SummUpAllNumbersInSchematic(filePath);
+
+            // Assert
+
+            result.Should().Be(4533);
+        }
+
+        [Fact]
+        public void Should_extend_given_file_with_dots()
+        {
+            // Arrange
+            string filePath1 = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\singleDigit.txt";
+            string filePath2 = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\singleDigitExtended.txt";
+            var extendedSchema = schematic.ReadFileToList(filePath2);
+
+            // Act
+            List<string> result = schematic.ExtendSchematic(filePath1);
+
+            // Assert
+            result.Should().BeEquivalentTo(extendedSchema);
+
+        }
+
+        [Fact]
+        public void Is_the_number_first()
+        {
+            // Arrange
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\trippleDigitExtended.txt";
+            var extendedSchema = schematic.ReadFileToList(filePath);
+            int i = 1;
+            int j = 1;
+
+            // Act
+            bool result = schematic.IsTheNumberFirst(extendedSchema, i, j);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Is_the_number_middle()
+        {
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\trippleDigitExtended.txt";
+            var extendedSchema = schematic.ReadFileToList(filePath);
+            int i = 1;
+            int j = 2;
+
+            bool result = schematic.IsTheNumberMiddle(extendedSchema, i, j);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Is_the_number_last()
+        {
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\trippleDigitExtended.txt";
+            var extendedSchema = schematic.ReadFileToList(filePath);
+            int i = 1;
+            int j = 3;
+
+            bool result = schematic.IsTheNumberLast(extendedSchema, i, j);
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_return_lenght_off_all_numbers()
+        {
+            // Arrange
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\engineSchematicsExtended.txt";
+            List<int> expected = new List<int>
+            {
+                3, 3, 2, 3, 3, 2, 3, 3, 3, 3
+            };
+
+            // Act
+            List<int> result = schematic.GetLenghtOfAllNumbers(filePath);
+
+            result.Should().Equal(expected);
+
+            // Assert
+        }
+
+        [Fact]
+        public void single_digit_all_points_around()
+        {
+            // Arrange
+            string path = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\singleDigitExtended.txt";
+            int i = 1;
+            int j = 1;
+
+
+            // Act
+            bool result = schematic.Dimension1(path, i, j);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void double_digit_all_points_around()
+        {
+            // Arrange
+            string path = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\doubleDigitExtended.txt";
+            int i = 1;
+            int j = 2;
+
+
+            // Act
+            bool result = schematic.Dimension2(path, i, j);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void triple_digit_all_points_around()
+        {
+            // Arrange
+            string path = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day03Src\\files\\trippleDigitExtended.txt";
+            int i = 1;
+            int j = 3;
+
+
+            // Act
+            bool result = schematic.Dimension3(path, i, j);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+    }
+}
