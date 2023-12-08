4eda59c800dd702df6dab103a2b5be5426b9c13a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-08 15:31:45 +0100

    day08: init

---
+++ aoc/day08-haunted-wasteland/day08.cs
@@ -0,0 +1,213 @@
+﻿namespace src.day08_haunted_wasteland
+{
+    public class Network
+    {
+        public List<string> ReadFileToList(string filePath)
+        {
+            List<string> linesList = new List<string>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                linesList.AddRange(lines);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return linesList;
+        }
+
+        public string GetDirections(string filePath)
+        {
+            string firstLine = File.ReadLines(filePath).First();
+            return firstLine;
+        }
+
+        public List<List<string>> GetNodes(string filePath)
+        {
+            List<List<string>> result = new List<List<string>>();
+            List<string> data = ReadFileToList(filePath);
+
+            for (int i = 2; i < data.Count; i++)
+            {
+                List<string> helper = new List<string>
+                {
+                    data[i].Substring(0, 3),
+                    data[i].Substring(7, 3),
+                    data[i].Substring(12, 3)
+                };
+
+                result.Add(helper);
+            }
+
+            return result;
+        }
+
+        public string GetNextDestionation(int index, string input, string filePath)
+        {
+            List<List<string>> data = GetNodes(filePath);
+
+            if (input == "L")
+            {
+                return data[index][1];
+            }
+            else
+            {
+                return data[index][2];
+            }
+        }
+
+        public string GetFinalLocation(string nextIteration, string filePath)
+        {
+            List<List<string>> data = GetNodes(filePath);
+            string directions = GetDirections(filePath);
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                string direction = char.ToString(directions[i]);
+
+                for (int j = 0; j < data.Count; j++)
+                {
+                    if (nextIteration == data[j][0])
+                    {
+                        nextIteration = GetNextDestionation(j, direction, filePath);
+                        break;
+                    }
+                }
+            }
+
+            return nextIteration;
+        }
+
+        public int GetNumberOfSteps(string filePath)
+        {
+            List<List<string>> data = GetNodes(filePath);
+
+            string startLocation = "AAA";
+            string destinationEnd = "ZZZ";
+
+            string directions = GetDirections(filePath);
+
+            int length = directions.Length;
+            string currentLocation = startLocation;
+
+            int iterations = 1;
+            while (currentLocation != destinationEnd)
+            {
+                if (iterations == 1)
+                {
+                    currentLocation = GetFinalLocation(startLocation, filePath);
+                }
+                else
+                {
+                    currentLocation = GetFinalLocation(currentLocation, filePath);
+                }
+
+                if (currentLocation == destinationEnd)
+                {
+                    break;
+                }
+
+                iterations++;
+            }
+
+            int result = length * iterations;
+            return result;
+        }
+
+        public int GetNumberOfStepsPart2(string startLocation, string filePath)
+        {
+            List<List<string>> data = GetNodes(filePath);
+
+            string directions = GetDirections(filePath);
+
+            int length = directions.Length;
+            string currentLocation = startLocation;
+
+            int iterations = 1;
+            while (currentLocation[2] != 'Z')
+            {
+                if (iterations == 1)
+                {
+                    currentLocation = GetFinalLocation(startLocation, filePath);
+                }
+                else
+                {
+                    currentLocation = GetFinalLocation(currentLocation, filePath);
+                }
+
+                if (currentLocation[2] == 'Z')
+                {
+                    break;
+                }
+
+                iterations++;
+            }
+
+            int result = length * iterations;
+            return result;
+        }
+
+        public ulong GetGCD(ulong a, ulong b)
+        {
+            while (b != 0)
+            {
+                ulong temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public ulong GetLCM(ulong firstNumber, ulong secondNumber)
+        {
+            return firstNumber * secondNumber / GetGCD(firstNumber, secondNumber);
+        }
+
+        public List<string> GetNodesThatEndOnA(string filePath)
+        {
+            List<List<string>> data = GetNodes(filePath);
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i][0][2] == 'A')
+                {
+                    result.Add(data[i][0]);
+                }
+            }
+
+            return result;
+        }
+
+        public List<int> GetIndividualNumbers(string filePath)
+        {
+            List<string> data = GetNodesThatEndOnA(filePath);
+            List<int> result = new List<int>();
+
+            foreach (string node in data)
+            {
+                result.Add(GetNumberOfStepsPart2(node, filePath));
+            }
+
+            return result;
+        }
+
+        public ulong GetLCMFinalAnswer(string filePath)
+        {
+            List<int> numbers = GetIndividualNumbers(filePath);
+
+            ulong lcmResult = (ulong)numbers[0];
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                lcmResult = (ulong)GetLCM(lcmResult, (ulong)numbers[i]);
+            }
+
+            return lcmResult;
+        }
+    }
+}
---
+++ test/day08-haunted-wasteland/day08test.cs
@@ -0,0 +1,184 @@
+﻿using FluentAssertions;
+using src.day08_haunted_wasteland;
+using Xunit;
+
+namespace test.day08_haunted_wasteland
+{
+    public class day08test
+    {
+        Network newNetwork = CreateNetwork();
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example1.txt", "RL")]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example2.txt", "LLR")]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\realNetwork.txt", "LRRLRRRLLRRRLRRRLLRRLRRRLRRLRRLRLRLRLRLRLLRRRLRRLRLRRRLRRRLRLRRRLRLRRLRRRLRRRLRLLRRRLRLLLRLRRRLRLRRLRRLLLLRRLRRLRLRLRRLRLRRLRRRLRRRLRLRLRRLLLLRRLRLRRLLRRRLRLRLRLRRRLRLLLRLRLRRRLRLRRRLRRRLRRRLLRRLRRRLRRRLRRRLRRRLRLLRRRLRLRRRLRLRLRRRLRRLRRLLRRRLRRRLRRRLRLRLRLRRLRRRLRRLRLRLRLRRRR")]
+        public void Should_return_directions(string filePath, string expected)
+        {
+            // Act
+            var result = newNetwork.GetDirections(filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example1.txt")]
+        public void Should_return_nodes(string filePath)
+        {
+            // Arrange
+            List<List<string>> expected = new List<List<string>>
+            {
+                new List<string>{"AAA", "BBB", "CCC"},
+                new List<string>{"BBB", "DDD", "EEE"},
+                new List<string>{"CCC", "ZZZ", "GGG"},
+                new List<string>{"DDD", "DDD", "DDD"},
+                new List<string>{"EEE", "EEE", "EEE"},
+                new List<string>{"GGG", "GGG", "GGG"},
+                new List<string>{"ZZZ", "ZZZ", "ZZZ"},
+            };
+
+            // Act
+            var result = newNetwork.GetNodes(filePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData(0, "R", "CCC")]
+        [InlineData(2, "L", "ZZZ")]
+        public void Should_return_correct_way(int index, string input, string expected)
+        {
+            // Arrange
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example1.txt";
+
+            // Act
+            string result = newNetwork.GetNextDestionation(index, input, filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0, "L", "BBB")]
+        [InlineData(1, "L", "AAA")]
+        [InlineData(0, "R", "BBB")]
+        [InlineData(1, "R", "ZZZ")]
+        public void Should_return_correct_way2(int index, string input, string expected)
+        {
+            // Arrange
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example2.txt";
+
+            // Act
+            string result = newNetwork.GetNextDestionation(index, input, filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example1.txt", "AAA", "ZZZ")]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example2.txt", "AAA", "BBB")]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example2.txt", "BBB", "ZZZ")]
+        public void Should_return_final_path(string filePath, string nextIteration, string expected)
+        {
+            // Act
+            string result = newNetwork.GetFinalLocation(nextIteration, filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example1.txt", 2)]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\example2.txt", 6)]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\realNetwork.txt", 16897)]
+        public void Should_return_final_number_of_steps(string filePath, int expected)
+        {
+            // Act
+            int result = newNetwork.GetNumberOfSteps(filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(10, 15, 5)]
+        [InlineData(48, 36, 12)]
+        [InlineData(356, 242, 2)]
+        public void Should_return_gcd(ulong firstNumber, ulong secondNumber, ulong expected)
+        {
+            ulong result = newNetwork.GetGCD(firstNumber, secondNumber);
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(10, 15, 30)]
+        [InlineData(4, 5, 20)]
+        [InlineData(18, 24, 72)]
+        public void Should_return_lcm(ulong firstNumber, ulong secondNumber, ulong expected)
+        {
+            ulong result = newNetwork.GetLCM(firstNumber, secondNumber);
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\realNetwork.txt")]
+        public void Should_return_all_nodes_that_ends_with_A(string filePath)
+        {
+            // Arrange
+            List<string> expected = new List<string>
+            {
+                "MCA",
+                "AAA",
+                "DCA",
+                "LGA",
+                "NLA",
+                "VPA"
+            };
+
+
+            // Act
+            List<string> result = newNetwork.GetNodesThatEndOnA(filePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\realNetwork.txt")]
+        public void Should_return_result_for_all_nodes_that_end_with_A(string filePath)
+        {
+            // Arrange
+            List<int> expected = new List<int>
+            {
+                16343,16897,20221,18559,11911,21883
+            };
+
+
+            // Act
+            List<int> result = newNetwork.GetIndividualNumbers(filePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\code\\day08\\data\\realNetwork.txt")]
+        public void Should_for_puzzle2_lcm(string filePath)
+        {
+            // Act
+            ulong result = newNetwork.GetLCMFinalAnswer(filePath);
+
+            // Assert
+            result.Should().Be(16563603485021);
+        }
+
+        private static Network CreateNetwork()
+        {
+            return new Network();
+        }
+    }
+}
