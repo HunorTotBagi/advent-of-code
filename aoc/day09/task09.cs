6cbfcef9bb2e4cf9ef1b16d0633513d815860273
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-08 16:29:45 +0100

    task09: init

---
+++ aoc/day09/task09.cs
@@ -0,0 +1,15 @@
+﻿namespace src.day09
+{
+    public class task09
+    {
+        public List<string> ReadFileToList(string filePath)
+        {
+            List<string> linesList = new List<string>();
+
+            string[] lines = File.ReadAllLines(filePath);
+            linesList.AddRange(lines);
+
+            return linesList;
+        }
+    }
+}
---
+++ test/day9/task09test.cs
@@ -0,0 +1,28 @@
+﻿using src.day08_haunted_wasteland;
+using src.day09;
+using Xunit;
+
+namespace test.day9
+{
+    public class task09test
+    {
+        task09 dummy = Createtask09();
+
+        [Theory]
+        [InlineData("file.txt")]
+        public void Should(string fileName)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day08-haunted-wasteland/data/" + fileName;
+
+            // Act
+
+            // Assert
+        }
+
+        private static task09 Createtask09()
+        {
+            return new task09();
+        }
+    }
+}
