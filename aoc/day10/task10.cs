bc6eed22f24e15d6cb42750d2dc02e283721b574
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-10 06:38:36 +0100

    feat: ReadTextFile feat: GetIndexOfStartingPosition feat: CreateZeroMatrix feat: CreateZeroMatrixForPipe

--- aoc/day10/task10.cs
+++ aoc/day10/task10.cs
@@ -1,6 +1,85 @@
 ﻿namespace src.day10
 {
-    public class task10
+    public class Pipe
     {
+        public List<int> GetIndexOfStartingPosition(string filePath)
+        {
+            List<List<char>> matrix = ReadTextFile(filePath);
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                for (int j = 0; j < matrix[0].Count; j++)
+                {
+                    if (matrix[i][j] == 'S')
+                    {
+                        return new List<int> { i, j};
+                    }
+                }
+            }
+            return new List<int> { -1, -1 };
+        }
+
+        public List<List<char>> ReadTextFile(string filePath)
+        {
+            List<List<char>> grid = new List<List<char>>();
+
+            try
+            {
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    // Create a list to hold characters in each line
+                    List<char> row = new List<char>();
+
+                    // Iterate through characters in the line and add them to the list
+                    foreach (char symbol in line)
+                    {
+                        row.Add(symbol);
+                    }
+
+                    // Add the list of characters to the grid
+                    grid.Add(row);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return grid;
+        }
+
+        static List<List<int>> CreateZeroMatrix(int rows, int columns)
+        {
+            List<List<int>> matrix = new List<List<int>>();
+
+            for (int i = 0; i < rows; i++)
+            {
+                List<int> row = new List<int>();
+
+                for (int j = 0; j < columns; j++)
+                {
+                    // Add zero to each element in the row
+                    row.Add(0);
+                }
+
+                // Add the row to the matrix
+                matrix.Add(row);
+            }
+
+            return matrix;
+        }
+
+        public List<List<int>> CreateZeroMatrixForPipe(string filePath)
+        {
+            List<List<char>> matrix = ReadTextFile(filePath);
+
+            int rows = matrix.Count;
+            int columns = matrix[0].Count;
+
+            return CreateZeroMatrix(rows, columns);
+        }
     }
 }
--- test/day10/task10test.cs
+++ test/day10/task10test.cs
@@ -1,11 +1,13 @@
-﻿using src.day10;
+﻿using System.ComponentModel;
+using FluentAssertions;
+using src.day10;
 using Xunit;
 
 namespace test.day10
 {
     public class task10test
     {
-        task10 newTask10 = CreateTask10();
+        Pipe newPipe = CreatePipe();
 
         string filePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day10\\data\\exampleData.txt";
         string realFilePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day10\\data\\realData.txt";
@@ -14,15 +16,58 @@ namespace test.day10
         public void First()
         {
             // Arrange
+            List<List<char>> expected = new List<List<char>>
+            {
+                new List<char> { '.', '.', '.', '.', '.'},
+                new List<char> { '.', 'S', '-', '7', '.',},
+                new List<char> { '.', '|', '.', '|', '.'},
+                new List<char> { '.', 'L', '-', 'J', '.'},
+                new List<char> { '.', '.', '.', '.', '.'},
+            };
 
             // Act
+            List<List<char>> result = newPipe.ReadTextFile(filePath);
 
             // Assert
+            result.Should().BeEquivalentTo(expected);
         }
 
-        private static task10 CreateTask10()
+        [Fact]
+        public void Should_find_the_index_of_S()
         {
-            return new task10();
+            // Arrange
+            List<int> expected = new List<int> { 1, 1 };
+
+            // Act
+            List<int> indexOfS = newPipe.GetIndexOfStartingPosition(filePath);
+
+            // Assert
+            indexOfS.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_create_matrix_of_all_zeros()
+        {
+            // Arrange
+            List<List<int>> expected = new List<List<int>>
+            {
+                new List<int> { 0, 0, 0, 0, 0},
+                new List<int> { 0, 0, 0, 0, 0},
+                new List<int> { 0, 0, 0, 0, 0},
+                new List<int> { 0, 0, 0, 0, 0},
+                new List<int> { 0, 0, 0, 0, 0},
+            };
+
+            // Act
+            List<List<int>> result = newPipe.CreateZeroMatrixForPipe(filePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        private static Pipe CreatePipe()
+        {
+            return new Pipe();
         }
     }
 }
