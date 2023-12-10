6a62d8a4256200daed0ccecf44205df94c85f346
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-10 16:12:27 +0100

    task06: init

---
+++ aoc/day06-wait-for-it/task06.cs
@@ -0,0 +1,81 @@
+﻿namespace src.day06_wait_for_it
+{
+    public class Race
+    {
+        public List<ulong> GetTimes(string filePath)
+        {
+            string startsWith = "Time:";
+            List<ulong> timeValues = ExtractTimeValues(filePath, startsWith);
+            return timeValues;
+        }
+
+        public List<ulong> GetDistances(string filePath)
+        {
+            string startsWith = "Distance:";
+            List<ulong> timeValues = ExtractTimeValues(filePath, startsWith);
+            return timeValues;
+        }
+
+        public List<ulong> ExtractTimeValues(string filePath, string startsWith)
+        {
+            List<ulong> timeValues = new List<ulong>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            string timeLine = lines.FirstOrDefault(line => line.StartsWith(startsWith));
+
+            if (timeLine != null)
+            {
+                string[] words = timeLine.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 1; i < words.Length; i++)
+                {
+                    if (ulong.TryParse(words[i], out ulong timeValue))
+                    {
+                        timeValues.Add(timeValue);
+                    }
+
+                }
+            }
+
+            return timeValues;
+        }
+
+        public ulong CalculateDifferentWaysForSpecificRace(string filePath, int raceNumber)
+        {
+            List<ulong> times = GetTimes(filePath);
+            List<ulong> distances = GetDistances(filePath);
+
+            List<ulong> result = new List<ulong>();
+
+            ulong firstTime = times[raceNumber];
+            ulong counter = times[raceNumber];
+            ulong firstDistance = distances[raceNumber];
+
+            for (ulong i = 0; i < firstTime; i++)
+            {
+                if (i * counter > firstDistance)
+                {
+                    result.Add(i * counter);
+                }
+                counter -= 1;
+            }
+
+            return (ulong)result.Count();
+        }
+
+        public ulong GetMarginError(string filePath)
+        {
+            List<ulong> times = GetTimes(filePath);
+
+            ulong result = 1;
+
+            for (ulong i = 0; i < (ulong)times.Count; i++)
+            {
+                result *= CalculateDifferentWaysForSpecificRace(filePath, (int)i);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/day06-wait-for-it/task06test.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Dtest.day06_wait_for_it
+{
+    public class task06test
+    {
+        Race newRace = CreateRace();
+
+        [Theory]
+        [InlineData("exampleRace.txt", new ulong[] { 7, 15, 30 })]
+        [InlineData("realRace.txt", new ulong[] { 48, 87, 69, 81 })]
+        [InlineData("exampleRacePuzzle2.txt", new ulong[] { 71530 })]
+        [InlineData("realRacePuzzle2.txt", new ulong[] { 48876981 })]
+        public void Should_get_times(string fileName, ulong[] expected)
+        {
+            // Arrange
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day06Src/data/" + fileName;
+
+            // Act
+            List<ulong> result = newRace.GetTimes(filePath);
+
+            // Assert
+            result.Should().Equal(expected);
+        }
+
+        [Theory]
+        [InlineData("exampleRace.txt", new ulong[] { 9, 40, 200 })]
+        [InlineData("realRace.txt", new ulong[] { 255, 1288, 1117, 1623 })]
+        [InlineData("exampleRacePuzzle2.txt", new ulong[] { 940200 })]
+        [InlineData("realRacePuzzle2.txt", new ulong[] { 255128811171623 })]
+        public void Should_get_distances(string fileName, ulong[] expected)
+        {
+            // Arrange
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day06Src/data/" + fileName;
+
+            // Act
+            List<ulong> result = newRace.GetDistances(filePath);
+
+            // Assert
+            result.Should().Equal(expected);
+        }
+
+        [Theory]
+        [InlineData("exampleRace.txt", 0, 4)]
+        [InlineData("exampleRace.txt", 1, 8)]
+        [InlineData("exampleRace.txt", 2, 9)]
+        [InlineData("exampleRacePuzzle2.txt", 0, 71503)]
+        [InlineData("realRacePuzzle2.txt", 0, 36992486)]
+        public void Should_return_number_of_different_ways(string fileName, int raceNumber, ulong expected)
+        {
+            // Arrange
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day06Src/data/" + fileName;
+
+            // Act
+            ulong result = newRace.CalculateDifferentWaysForSpecificRace(filePath, raceNumber);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("exampleRace.txt", 288)]
+        [InlineData("realRace.txt", 252000)]
+        public void Should_return_margin_of_error(string fileName, ulong expected)
+        {
+            // Arrange
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day06Src/data/" + fileName;
+
+            // Act
+            ulong result = newRace.GetMarginError(filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Race CreateRace()
+        {
+            return new Race();
+        }
+    }
+}
