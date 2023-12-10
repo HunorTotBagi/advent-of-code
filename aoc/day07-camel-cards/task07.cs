26a9a394f507747d68bcced4899f188ccd38ab7a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-10 19:01:11 +0100

    feat: GetCardType

--- aoc/day07-camel-cards/task07.cs
+++ aoc/day07-camel-cards/task07.cs
@@ -1,12 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Reflection.Metadata;
 
 namespace src.day07
 {
-    internal class task07
+    //var lines = File.ReadAllLines("data/exampleData0.txt");
+    public class Hand
     {
+
+        //List<char> CardRanks = { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+        public Dictionary<string, int> ReadFile(string filePath)
+        {
+            var lines = File.ReadAllLines("C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day07-camel-cards\\data\\exampleData0.txt");
+
+            Dictionary<string, int> data = new();
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ');
+                data[parts[0]] = int.Parse(parts[1]);   
+            }
+            return data;
+        }
+
+
+        public Dictionary<char, int> CountOccurences(string card)
+        {
+            Dictionary<char, int> result = new();
+
+            foreach (char c in card)
+            {
+                if (result.ContainsKey(c))
+                {
+                    result[c] += 1;
+                }
+                else
+                {
+                    result[c] = 1;
+                }
+            }
+
+            return result;
+        }
+
+        public int GetCardType(string card)
+        {
+            Dictionary<char, int> counts = CountOccurences(card);
+
+            if (counts.ContainsValue(5))
+                return 7; // Five of a kind
+
+            if (counts.ContainsValue(4) && counts.ContainsValue(1))
+                return 6; // Four of a kind
+
+            if (counts.ContainsValue(3) && counts.ContainsValue(2))
+                return 5; // Full house
+
+            if (counts.ContainsValue(3) && counts.ContainsValue(1))
+                return 4; // Three of a kind
+
+            if (counts.Count(kv => kv.Value == 2) == 2 && counts.ContainsValue(1))
+                return 3; // Two pair
+
+            if (counts.ContainsValue(2) && counts.ContainsValue(1))
+                return 2; // One pair
+
+            return 1;
+        }
     }
 }
--- test/day07-camel-cards/task07test.cs
+++ test/day07-camel-cards/task07test.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using src.day07;
+using Xunit;
 
 namespace test.day07
 {
-    internal class task07test
+    public class task07test
     {
+        Hand newHand = CreateHand();
+
+        [Fact]
+        public void Should_read_in_file()
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day11/data/exampleData0.txt";
+            Dictionary<string, int> expected = new Dictionary<string, int>
+            {
+                { "32T3K" , 765 },
+                { "T55J5" , 684 },
+                { "KK677" , 28 },
+                { "KTJJT" , 220 },
+                { "QQQJA" , 483 },
+            };
+
+            // Act
+            Dictionary<string, int> result = newHand.ReadFile(filePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("AAAAA", new char[] { 'A' }, new int[] { 5 })]
+        [InlineData("AA8AA", new char[] { 'A', '8' }, new int[] { 4, 1 })]
+        [InlineData("23332", new char[] { '2', '3' }, new int[] { 2, 3 })]
+        [InlineData("TTT98", new char[] { 'T', '9', '8' }, new int[] { 3, 1, 1 })]
+        [InlineData("23432", new char[] { '2', '3', '4' }, new int[] { 2, 2, 1 })]
+        [InlineData("A23A4", new char[] { 'A', '2', '3', '4' }, new int[] { 2, 1, 1, 1 })]
+        [InlineData("23456", new char[] { '2', '3', '4', '5', '6' }, new int[] { 1, 1, 1, 1, 1 })]
+        public void CountOccurences_ShouldReturnExpectedResult(string card, char[] expectedChars, int[] expectedCounts)
+        {
+            // Arrange
+            Dictionary<char, int> expected = new Dictionary<char, int>();
+            for (int i = 0; i < expectedChars.Length; i++)
+            {
+                expected[expectedChars[i]] = expectedCounts[i];
+            }
+
+            // Act
+            Dictionary<char, int> result = newHand.CountOccurences(card);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("AAAAA", 7)]
+        [InlineData("AA8AA", 6)]
+        [InlineData("23332", 5)]
+        [InlineData("TTT98", 4)]
+        [InlineData("23432", 3)]
+        [InlineData("A23A4", 2)]
+        [InlineData("23456", 1)]
+        public void Should_return_hand_type(string card, int expected)
+        {
+            // Act
+            int result = newHand.GetCardType(card);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Hand CreateHand()
+        {
+            return new Hand();
+        }
     }
 }
