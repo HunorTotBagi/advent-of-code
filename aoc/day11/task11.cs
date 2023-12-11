a72bdee264e05f54f64b902978fb7486e25ef5f8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-11 06:51:25 +0100

    feat: ExpandGalaxy

--- aoc/day11/task11.cs
+++ aoc/day11/task11.cs
@@ -2,6 +2,108 @@
 {
     public class Hunor
     {
+        public List<List<char>> ExpanGalaxy(string filePath0)
+        {
+            List<List<char>> matrix = ExpandRows(filePath0);
 
+            List<List<char>> result = new List<List<char>>();
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                result.Add(new List<char>());
+            }
+
+            for (int j = 0; j < matrix[0].Count; j++)
+            {
+                int counter = 0;
+                for (int i = 0; i < matrix.Count; i++)
+                {
+                    if (matrix[i][j] == '.')
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == matrix.Count)
+                {
+                    for (int i = 0; i < matrix.Count; i++)
+                    {
+                        result[i].Add('.');
+                        result[i].Add('.');
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < matrix.Count; i++)
+                    {
+                        result[i].Add(matrix[i][j]);
+                    }
+                }
+            }
+
+            return result; // Added return statement
+        }
+
+        public List<List<char>> ExpandRows(string filePath0)
+        {
+            List<List<char>> matrix = ReadFileToGrid(filePath0);
+
+            List<List<char>> result = new();
+
+            List<char> madeUpRows = new();
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                madeUpRows.Add('.');
+            }
+
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                int counter = 0;
+                for (int j = 0; j < matrix[0].Count; j++)
+                {
+                    if (matrix[i][j] == '.')
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == matrix[0].Count)
+                {
+                    result.Add(madeUpRows);
+                    result.Add(madeUpRows);
+                }
+                else
+                {
+                    result.Add(matrix[i]);
+                }
+            }
+
+            return result;
+        }
+
+        public List<List<char>> ReadFileToGrid(string filePath)
+        {
+            List<List<char>> grid = new List<List<char>>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        List<char> row = new List<char>(line.ToCharArray());
+                        grid.Add(row);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading the file: {ex.Message}");
+            }
+
+            return grid;
+        }
     }
+
+
 }
--- test/day11/task11test.cs
+++ test/day11/task11test.cs
@@ -1,4 +1,5 @@
-﻿using src.day11;
+﻿using FluentAssertions;
+using src.day11;
 using Xunit;
 
 namespace test.day11
@@ -6,31 +7,53 @@ namespace test.day11
     public class task11test
     {
         string filePath0 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11\\data\\exampleData0.txt";
-        string filePath1 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11\\data\\exampleData1.txt";
+        string filePath1 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11\\data\\expandedGalaxyRows.txt";
         string filePath2 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11\\data\\exampleData2.txt";
         string realFilePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11\\data\\realData.txt";
 
-        [Theory]
-        [InlineData("asd")]
-        public void Should1(string fileName)
+        Hunor newGalaxy = new Hunor();
+
+        [Fact]
+        public void Should_expand_the_galaxy_rows()
         {
             // Arrange
-            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day11/data/" + fileName;
+            List<List<char>> expected = newGalaxy.ReadFileToGrid(filePath1);
 
             // Act
-
+            List<List<char>> result = newGalaxy.ExpandRows(filePath0);
             // Assert
+            result.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
-        public void Should()
+        public void Should_expand_the_galaxy_columns()
         {
             // Arrange
+            List<List<char>> expected = newGalaxy.ReadFileToGrid(filePath2);
 
             // Act
-
+            List<List<char>> result = newGalaxy.ExpanGalaxy(filePath0);
             // Assert
+            result.Should().BeEquivalentTo(expected);
         }
+
+
+
+
+
+        //[Theory]
+        //[InlineData("asd")]
+        //public void Should1(string fileName)
+        //{
+        //    // Arrange
+        //    var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day11/data/" + fileName;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+
 
         private static Hunor Create()
         {
