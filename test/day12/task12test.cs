8d6157b495e38139e6dbbbf4ffc659c9fce05bf0
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-11 14:13:55 +0100

    task12: init

---
+++ test/day12/task12test.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using src.day12;
+using Xunit;
+
+namespace test.day12
+{
+    public class ItemTest
+    {
+        string filePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12\\data\\exampleData.txt";
+        string filePath1 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12\\data\\exampleData1.txt";
+        string filePath2 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12\\data\\exampleData2.txt";
+        string realFilePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12\\data\\realData.txt";
+
+        Item newItem = CreateItem();
+
+        [Fact]
+        public void Should()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+
+        private static Item CreateItem()
+        {
+            return new Item();
+        }
+    }
+}
