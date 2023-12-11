31dad0388feeee4aad3f01ede15eeecb04539bdf
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-11 10:07:38 +0100

    refactor & todo: rename

---
+++ aoc/day11-cosmic-expansion/task11.cs
@@ -0,0 +1,102 @@
+﻿namespace src.day11
+{
+    public class Galaxy
+    {
+        private List<List<char>> galaxy;
+
+        public Galaxy()
+        {
+            galaxy = ReadFileToGrid("C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11-cosmic-expansion\\data\\inputExample.txt");
+        }
+        public List<List<ulong>> GetCoordinates()
+        {
+            List<List<ulong>> coordinates = new List<List<ulong>>();
+
+            for (int i = 0; i < galaxy.Count; i++)
+            {
+                for (int j = 0; j < galaxy[0].Count; j++)
+                {
+                    if (galaxy[i][j] == '#')
+                    {
+                        coordinates.Add(new List<ulong> { (ulong)i, (ulong)j });
+                    }
+                }
+            }
+            return coordinates;
+        }
+
+        public bool EmptyRow(ulong rowIndex)
+        {
+            int rowIndexInt = (int)rowIndex;
+            return galaxy[rowIndexInt].All(cell => cell == '.');
+        }
+
+        public bool EmptyCol(ulong colIndex)
+        {
+            int colIndexInt = (int)colIndex;
+            return galaxy.All(row => row[colIndexInt] == '.');
+        }
+
+        public ulong GetDistanceBetweenTwoPoints(List<ulong> firstPoint, List<ulong> secondPoint, ulong expansion)
+        {
+            ulong x1 = firstPoint[0], y1 = firstPoint[1];
+            ulong x2 = secondPoint[0], y2 = secondPoint[1];
+
+            (x1, x2) = (Math.Min(x1, x2), Math.Max(x1, x2));
+            (y1, y2) = (Math.Min(y1, y2), Math.Max(y1, y2));
+
+            ulong result = 0;
+            for (ulong i = x1; i < x2; i++)
+            {
+                result += 1;
+                if (EmptyRow(i))
+                {
+                    result += expansion - 1;
+                }
+            }
+            for (ulong j = y1; j < y2; j++)
+            {
+                result += 1;
+                if (EmptyCol(j))
+                {
+                    result += expansion - 1;
+                }
+            }
+            return result;
+        }
+
+        public ulong GetFinal(ulong expansion)
+        {
+            ulong result = 0;
+
+            List<List<ulong>> coordinates = GetCoordinates();
+
+            for (int i = 0; i < coordinates.Count; i++)
+            {
+                for (int j = i + 1; j < coordinates.Count; j++)
+                {
+                    result = result + GetDistanceBetweenTwoPoints(coordinates[i], coordinates[j], expansion);
+                }
+            }
+
+            return result;
+        }
+
+        public List<List<char>> ReadFileToGrid(string filePath)
+        {
+            List<List<char>> grid = new List<List<char>>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<char> row = new List<char>(line.ToCharArray());
+                    grid.Add(row);
+                }
+            }
+
+            return grid;
+        }
+    }
+}
--- test/day11-cosmic-expansion/task11test.cs
+++ test/day11-cosmic-expansion/task11test.cs
@@ -6,19 +6,24 @@ namespace test.day11
 {
     public class task11test
     {
-        Galaxy newGalaxy = new Galaxy();
+        Galaxy newGalaxy = CreateGalaxy();
 
         [Fact]
         public void Should_Get_coordinates()
         {
             // Arrange
-            List<List<ulong>> exp = new List<List<ulong>>();
+            List<List<ulong>> expected = new List<List<ulong>>
+            {
+                new List<ulong> { 0, 3}, new List<ulong> { 1, 7},new List<ulong> { 2, 0},
+                new List<ulong> { 4, 6}, new List<ulong> { 5, 1},new List<ulong> { 6, 9},
+                new List<ulong> { 8, 7}, new List<ulong> { 9, 0},new List<ulong> { 9, 4},
+            };
 
             // Act
             List<List<ulong>> result = newGalaxy.GetCoordinates();
 
             // Assert
-            result.Should().BeEquivalentTo(exp);
+            result.Should().BeEquivalentTo(expected);
         }
 
         [Theory]
@@ -47,8 +52,9 @@ namespace test.day11
         {
             List<ulong> first = new List<ulong> { 0,3};
             List<ulong> second = new List<ulong> { 1, 7 };
+            ulong expansion = 2;
 
-            ulong result = newGalaxy.GetDistanceBetweenTwoPoints(first, second);
+            ulong result = newGalaxy.GetDistanceBetweenTwoPoints(first, second, expansion);
 
             result.Should().Be(6);
         }
@@ -56,11 +62,10 @@ namespace test.day11
         [Fact]
         public void Final()
         {
-            ulong result = newGalaxy.GetFinal();
+            ulong result = newGalaxy.GetFinal(2);
 
             result.Should().Be(374);
         }
-
 
         private static Galaxy CreateGalaxy()
         {
