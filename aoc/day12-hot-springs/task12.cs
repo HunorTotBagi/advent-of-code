09897459eedebacaeb897de92b3be0d30f455d4d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-12 07:53:51 +0100

    task12 - feat: ReadFile

---
+++ aoc/day12-hot-springs/task12.cs
@@ -0,0 +1,21 @@
+﻿namespace src.day12_hot_springs
+{
+    public class HotSpring
+    {
+        public (List<string>, List<List<int>>) ReadFile(string filePath)
+        {
+            List<string> stringsList = new List<string>();
+            List<List<int>> intsList = new List<List<int>>();
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ');
+                stringsList.Add(parts[0]);
+                List<int> intValues = parts[1].Split(',').Select(int.Parse).ToList();
+                intsList.Add(intValues);
+            }
+            return (stringsList, intsList);
+        }
+    }
+}
---
+++ test/day12-hot-springs/task12test.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using src.day12_hot_springs;
+using Xunit;
+
+namespace Day12_Hot_springs
+{
+    public class RecordTests
+    {
+        string filePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12-hot-springs\\data\\exampleData.txt";
+        string realFilePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12-hot-springs\\data\\realData.txt";
+
+        HotSpring newRecord = CreateHotSpring();
+
+        [Theory]
+        [InlineData("exampleData.txt")]
+        public void Should_return_symbols_and_numbers(string fileName)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day12-hot-springs/data/" + fileName;
+
+            var strings = new List<string>
+            {
+                { "???.###" }, { ".??..??...?##." }, { "?#?#?#?#?#?#?#?" },
+                { "????.#...#..."}, { "????.######..#####." }, { "?###????????" }
+            };
+
+            var integers = new List<List<int>>
+            {
+                new List<int>{ 1, 1, 3 }, new List<int>{ 1, 1, 3 }, new List<int>{ 1, 3, 1, 6 },
+                new List<int>{ 4, 1, 1 }, new List<int>{ 1, 6, 5 }, new List<int>{ 3, 2, 1 }
+            };
+
+            // Act
+            (List<string> stringResult, List<List<int>> intResult) = newRecord.ReadFile(filePath);
+
+            // Assert
+            stringResult.Should().BeEquivalentTo(strings);
+            intResult.Should().BeEquivalentTo(integers);
+        }
+
+
+
+
+
+        private static HotSpring CreateHotSpring()
+        {
+            return new HotSpring();
+        }
+    }
+}
