0a616b4fea454f4ae72789c3285bd84781a63346
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-15 08:43:48 +0100

    task15: refactor tests

--- day15-lens-library/task15test.cs
+++ day15-lens-library/task15test.cs
@@ -6,7 +6,7 @@ namespace TestProject1
 {
     public class task15tests
     {
-        Hunor newHunor = CreateHunor();
+        LensLibrary newLensLibrary = CreateLensLibrary();
 
         string testData = "C:\\Users\\htotbagi\\source\\repos\\advent-of-code-LATEST\\advent-of-code-LATEST\\data\\day15\\testData.txt";
         string realData = "C:\\Users\\htotbagi\\source\\repos\\advent-of-code-LATEST\\advent-of-code-LATEST\\data\\day15\\realData.txt";
@@ -15,15 +15,15 @@ namespace TestProject1
         [InlineData('H', 72)]
         [InlineData('A', 65)]
         [InlineData('S', 83)]
-
-        public void Test0(char input, int expected)
+        public void Should_return_characters_ASCII_code(char inputCharacter, int ASCIICode)
         {
             // Arrange
 
             // Act
-            int result = newHunor.ConvertToAscii(input);
+            int result = newLensLibrary.ConvertToAscii(inputCharacter);
+
             // Assert
-            result.Should().Be(expected);
+            result.Should().Be(ASCIICode);
         }
 
         [Theory]
@@ -31,53 +31,40 @@ namespace TestProject1
         [InlineData("rn=1", 30)]
         [InlineData("cm-", 253)]
         [InlineData("qp=3", 97)]
-        [InlineData("cm=2", 47)]
-        [InlineData("qp-", 14)]
-        [InlineData("pc=4", 180)]
-        [InlineData("ot=9", 9)]
-        [InlineData("ab=5", 197)]
-        [InlineData("pc-", 48)]
-        [InlineData("pc=6", 214)]
-        [InlineData("ot=7", 231)]
-        [InlineData("rn", 0)]
-        [InlineData("qp", 1)]
-        public void Test1(string input, int expected)
+        public void Should_return_HASHAlgorithm_result(string input, int expected)
         {
             // Arrange
 
             // Act
-            int result = newHunor.HASHAlgo(input);
+            int result = newLensLibrary.HASHAlgorithm(input);
+
             // Assert
             result.Should().Be(expected);
         }
 
         [Fact]
-        public void Should_read()
+        public void Should_return_result_for_puzzle_1_test_data()
         {
-            List<string> expected = new List<string>();
+            // Arrange
+            List<string> input = newLensLibrary.ReadFile(testData);
 
-            List<string> result = newHunor.ReadFile(testData);
+            // Act
+            int result = newLensLibrary.Final(input);
 
-            result.Should().BeEquivalentTo(expected);
-        }
-
-        [Fact]
-        public void Shoudl_syn()
-        {
-            List<string> input = newHunor.ReadFile(testData);
-
-            int result = newHunor.Final(input);
-
+            // Assert
             result.Should().Be(1320);
         }
 
         [Fact]
-        public void Shoudl_syn1111()
+        public void Should_return_result_for_puzzle_1_real_data()
         {
-            List<string> input = newHunor.ReadFile(realData);
+            // Arrange
+            List<string> input = newLensLibrary.ReadFile(realData);
 
-            int result = newHunor.Final(input);
+            // Act
+            int result = newLensLibrary.Final(input);
 
+            // Assert
             result.Should().Be(512950);
         }
 
@@ -86,23 +73,29 @@ namespace TestProject1
         [InlineData("cm-", 0)]
         [InlineData("qp=3", 1)]
         [InlineData("ot=7", 3)]
-        public void Should_retur_label_ascyy(string input, int expected)
+        public void Should_return_box_index(string input, int expected)
         {
+            // Arrange
 
-            int result = newHunor.GetBoxIndex(input);
+            // Act
+            int result = newLensLibrary.GetBoxIndex(input);
 
+            // Assert
             result.Should().Be(expected);
         }
 
         [Theory]
         [InlineData("rngwergw=1", true)]
         [InlineData("qpasd=3", true)]
-        [InlineData("ottt=7", true)]
         [InlineData("cm-1", false)]
-        public void Should_return_true_if_equal(string input, bool exp)
+        public void Should_return_true_if_string_contains_equal_symbol(string input, bool exp)
         {
-            bool result = newHunor.HaveEqual(input);
+            // Arrange
 
+            // Act
+            bool result = newLensLibrary.StringContainsEqualSymbol(input);
+
+            // Assert
             result.Should().Be(exp);
         }
 
@@ -110,10 +103,14 @@ namespace TestProject1
         [InlineData("rngwergw-1", true)]
         [InlineData("qpasd=3", false)]
         [InlineData("ottt-7", true)]
-        public void Should_return_true_if_mins(string input, bool exp)
+        public void Should_return_true_if_string_contains_minus_symbol(string input, bool exp)
         {
-            bool result = newHunor.HaveMinus(input);
+            // Arrange
 
+            // Act
+            bool result = newLensLibrary.StringContainsMinusSymbol(input);
+
+            // Assert
             result.Should().Be(exp);
         }
 
@@ -123,46 +120,44 @@ namespace TestProject1
         [InlineData("ottt-711", 711)]
         public void Should_return_number(string input, int exp)
         {
-            int result = newHunor.ExtractNumber(input);
+            // Arrange
 
+            // Act
+            int result = newLensLibrary.ExtractNumber(input);
+
+            // Assert
             result.Should().Be(exp);
         }
 
         [Fact]
-        public void Main_logic()
+        public void Should_return_result_for_puzzle_2_test_data()
         {
-            List<string> input = newHunor.ReadFile(testData);
-            List<List<string>> exp = new List<List<string>>();
-
-            List<List<string>> res = newHunor.PutInBox(input);
-
-            res.Should().BeEquivalentTo(exp);
-        }
-
-        [Fact]
-        public void Final_answer()
-        {
-            List<string> input = newHunor.ReadFile(testData);
+            // Arrange
+            List<string> input = newLensLibrary.ReadFile(testData);
             
-            int result = newHunor.FinalResult(input);
+            // Act
+            int result = newLensLibrary.GetFocusingPower(input);
 
+            // Assert
             result.Should().Be(145);  
         }
 
         [Fact]
-        public void Final_answer_real()
+        public void Should_return_result_for_puzzle_2_real_data()
         {
-            List<string> input = newHunor.ReadFile(realData);
+            // Arrange
+            List<string> input = newLensLibrary.ReadFile(realData);
 
-            int result = newHunor.FinalResult(input);
+            // Act
+            int result = newLensLibrary.GetFocusingPower(input);
 
+            // Assert
             result.Should().Be(247153);
         }
 
-
-        private static Hunor CreateHunor()
+        private static LensLibrary CreateLensLibrary()
         {
-            return new Hunor();
+            return new LensLibrary();
         }
     }
 }
