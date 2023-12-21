ac26601d7d5231853b68a6cab793deb31a3a1cd8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-21 12:49:11 +0100

    task16: refactor & add puzzle 2

--- day16-the-floor-will-be-lava/task16.cs
+++ day16-the-floor-will-be-lava/task16.cs
@@ -1,14 +1,16 @@
+using System.Data;
+
 namespace AdventOfCode2023.Day16
 {
     public class Contraption
     {
-        public int Calculate(string filePath)
+        public int Calculate(string filePath, int startRow, int startCol, int startRowDirection, int startColDirection)
         {
             var matrix = ReadFileIntoCharList(filePath);
 
             var que = new Queue<(int, int, int, int)>();
             var seen = new HashSet<(int, int, int, int)>();
-            que.Enqueue((0, -1, 0, 1));
+            que.Enqueue((startRow, startCol, startRowDirection, startColDirection));
 
             while (que.Count != 0)
             {
@@ -16,7 +18,7 @@ namespace AdventOfCode2023.Day16
                 row += rowDirection;
                 col += colDirection;
 
-                if (row < 0 || row >= matrix.Count || col < 0 || col >= matrix[0].Count)
+                if (IndexesOutOfBound(matrix, row, col))
                     continue;
 
                 char ch = matrix[row][col];
@@ -70,10 +72,48 @@ namespace AdventOfCode2023.Day16
                     }
                 }
             }
-            return seen.Select(x => (x.Item1, x.Item2)).Distinct().Count(); 
-    }
+            return seen.Select(x => (x.Item1, x.Item2)).Distinct().Count();
+        }
 
-    public List<List<char>> ReadFileIntoCharList(string filePath)
+        private static bool IndexesOutOfBound(List<List<char>> matrix, int row, int col)
+        {
+            return row < 0 || row >= matrix.Count || col < 0 || col >= matrix[0].Count;
+        }
+
+        public object CalculateFromEveryEdge(string filePath)
+        {
+            int maxValue = 0;
+            var matrix = ReadFileIntoCharList(filePath);
+
+            maxValue = CheckMaximumValuesForRow(filePath, maxValue, matrix);
+            maxValue = CheckMaximumValuesForColumns(filePath, maxValue, matrix);
+
+            return maxValue;
+        }
+
+        private int CheckMaximumValuesForColumns(string filePath, int maxValue, List<List<char>> matrix)
+        {
+            for (int i = 0; i < matrix.Count(); i++)
+            {
+                maxValue = Math.Max(maxValue, Calculate(filePath, -1, i, 1, 0));
+                maxValue = Math.Max(maxValue, Calculate(filePath, matrix.Count(), i, -1, 0));
+            }
+
+            return maxValue;
+        }
+
+        private int CheckMaximumValuesForRow(string filePath, int maxValue, List<List<char>> matrix)
+        {
+            for (int i = 0; i < matrix.Count(); i++)
+            {
+                maxValue = Math.Max(maxValue, Calculate(filePath, i, -1, 0, 1));
+                maxValue = Math.Max(maxValue, Calculate(filePath, i, matrix[0].Count(), 0, 1));
+            }
+
+            return maxValue;
+        }
+
+        public List<List<char>> ReadFileIntoCharList(string filePath)
         {
             List<List<char>> result = new List<List<char>>();
             string[] lines = File.ReadAllLines(filePath);
