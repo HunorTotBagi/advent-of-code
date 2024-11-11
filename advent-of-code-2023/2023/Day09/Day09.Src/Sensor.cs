2f22b17c3a8b9c3d49c0a7b26eb41e2f36ffdcfc
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-11 20:55:25 +0100

    refactor: 2023-Day09

---
+++ advent-of-code-2023/2023/Day09/Day09.Src/Sensor.cs
@@ -0,0 +1,92 @@
+﻿namespace Day09.Src;
+
+public class Sensor
+{
+    public List<int> GetNextRowCalculation(List<int> stringRow)
+    {
+        return stringRow.Zip(stringRow.Skip(1), (current, next) => next - current).ToList();
+    }
+
+    public List<List<int>> GetAllDifferencesForThatRow(List<int> row)
+    {
+        List<List<int>> storage = new List<List<int>> { row };
+
+        while (true)
+        {
+            List<int> nextRow = GetNextRowCalculation(storage[^1]);
+            storage.Add(nextRow);
+
+            if (nextRow.All(value => value == 0))
+            {
+                break;
+            }
+        }
+        return storage;
+    }
+
+    public int Extrapolate(List<List<int>> input)
+    {
+        input[^1].Add(0);
+
+        for (int i = input.Count - 1; i >= 1; i--)
+        {
+            int lastElement = input[i].Last();
+            input[i - 1].Add(input[i - 1].Last() + lastElement);
+        }
+        return input[0][^1];
+    }
+
+    public int ExtrapolateBackwards(List<List<int>> input)
+    {
+        input[^1].Add(0);
+
+        for (int i = input.Count - 1; i >= 1; i--)
+        {
+            int lastElement = input[i].Last();
+            input[i - 1].Add(input[i - 1].First() - lastElement);
+        }
+        return input[0][^1];
+    }
+
+
+    public int GetNextNumberInTheSequence(string filePath)
+    {
+        List<List<int>> data = newExtractor(filePath);
+
+        int result = 0;
+
+        foreach (List<int> row in data)
+        {
+            result += Extrapolate(GetAllDifferencesForThatRow(row));
+        }
+        return result;
+    }
+
+    public int GetNextNumberInTheSequenceBackwards(string filePath)
+    {
+        List<List<int>> data = newExtractor(filePath);
+
+        int result = 0;
+
+        foreach (List<int> row in data)
+        {
+            result += ExtrapolateBackwards(GetAllDifferencesForThatRow(row));
+        }
+        return result;
+    }
+
+
+    public List<List<int>> newExtractor(string filePath)
+    {
+        List<List<int>> result = ReadFile(filePath);
+
+        return result;
+    }
+
+    static List<List<int>> ReadFile(string filePath)
+    {
+        return File.ReadAllLines(filePath).Select(line => line.Split(' ')
+                                        .Select(value => int.TryParse(value, out int intValue) ? intValue : 0)
+                                        .ToList()).ToList();
+    }
+}
---
+++ advent-of-code-2023/2023/Day09/Day09.Test/Tests.cs
@@ -0,0 +1,254 @@
+using Day09.Src;
+using FluentAssertions;
+
+namespace Day09.Test;
+
+public class Tests
+{
+    Sensor newSensor = CreateSensor();
+
+    string filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/Data/exampleFile.txt";
+    string realFilePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/Data/realFile.txt";
+
+    [Fact]
+    public void Should_read_in_file()
+    {
+        // Arrange
+        List<List<int>> expected = new List<List<int>>
+            {
+                new List<int> { 0, 3, 6, 9, 12, 15 },
+                new List<int> { 1, 3, 6, 10, 15, 21 },
+                new List<int> { 10, 13, 16, 21, 30, 45 },
+            };
+
+        // Act
+        List<List<int>> result = newSensor.newExtractor(filePath);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_get_next_row1()
+    {
+        // Arrange
+        List<int> input = new List<int> { 0, 3, 6, 9, 12, 15 };
+        List<int> expected = new List<int> { 3, 3, 3, 3, 3 };
+
+        // Act
+        List<int> result = newSensor.GetNextRowCalculation(input);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_get_next_row2()
+    {
+        // Arrange
+        List<int> input = new List<int> { 3, 3, 3, 3, 3 };
+        List<int> expected = new List<int> { 0, 0, 0, 0 };
+
+        // Act
+        List<int> result = newSensor.GetNextRowCalculation(input);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_get_all_differences_for_specific_row0()
+    {
+        // Arrange
+        var input = newSensor.newExtractor(filePath)[0];
+        List<List<int>> expected = new List<List<int>>
+            {
+                new List<int>{ 0, 3, 6, 9, 12, 15 },
+                new List<int>{ 3, 3, 3, 3, 3 },
+                new List<int>{ 0, 0, 0, 0 },
+            };
+
+        // Act
+        List<List<int>> result = newSensor.GetAllDifferencesForThatRow(input);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_get_all_differences_for_specific_row1()
+    {
+        // Arrange
+        var input = newSensor.newExtractor(filePath)[1];
+
+        List<List<int>> expected = new List<List<int>>
+            {
+                new List<int>{ 1, 3, 6, 10, 15, 21 },
+                new List<int>{ 2, 3, 4, 5, 6 },
+                new List<int>{ 1, 1, 1, 1 },
+                new List<int>{ 0, 0, 0 },
+            };
+
+        // Act
+        List<List<int>> result = newSensor.GetAllDifferencesForThatRow(input);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_get_all_differences_for_specific_row2()
+    {
+        // Arrange
+        var input = newSensor.newExtractor(filePath)[2];
+
+        List<List<int>> expected = new List<List<int>>
+            {
+                new List<int>{ 10, 13, 16, 21, 30, 45},
+                new List<int>{ 3, 3, 5, 9, 15 },
+                new List<int>{ 0, 2, 4, 6 },
+                new List<int>{ 2, 2, 2 },
+                new List<int>{ 0, 0 },
+            };
+
+        // Act
+        List<List<int>> result = newSensor.GetAllDifferencesForThatRow(input);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_extrapolate_0()
+    {
+        // Arrange
+        var firstRow = newSensor.newExtractor(filePath)[0];
+        List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+        int expected = 18;
+
+        // Act
+        int result = newSensor.Extrapolate(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_extrapolate_1()
+    {
+        // Arrange
+        var firstRow = newSensor.newExtractor(filePath)[1];
+        List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+        int expected = 28;
+
+        // Act
+        int result = newSensor.Extrapolate(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_extrapolate_2()
+    {
+        // Arrange
+        var firstRow = newSensor.newExtractor(filePath)[2];
+        List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+        int expected = 68;
+
+        // Act
+        int result = newSensor.Extrapolate(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_extrapolate_backwards_0()
+    {
+        // Arrange
+        var firstRow = newSensor.newExtractor(filePath)[0];
+        List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+        int expected = -3;
+
+        // Act
+        int result = newSensor.ExtrapolateBackwards(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_extrapolate_backwards_1()
+    {
+        // Arrange
+        var firstRow = newSensor.newExtractor(filePath)[1];
+        List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+        int expected = 0;
+
+        // Act
+        int result = newSensor.ExtrapolateBackwards(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_extrapolate_backwards_2()
+    {
+        // Arrange
+        var firstRow = newSensor.newExtractor(filePath)[2];
+        List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+        int expected = 5;
+
+        // Act
+        int result = newSensor.ExtrapolateBackwards(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_return_sum_of_extrapolated_values_for_example_data()
+    {
+        // Arrange
+        int expected = 114;
+
+        // Act
+        int result = newSensor.GetNextNumberInTheSequence(filePath);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_return_sum_of_extrapolated_values_for_real_data()
+    {
+        // Arrange
+        int expected = 1581679977;
+
+        // Act
+        int result = newSensor.GetNextNumberInTheSequence(realFilePath);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_return_sum_of_extrapolated_values_backwards_for_real_data()
+    {
+        // Arrange
+        int expected = 889;
+
+        // Act
+        int result = newSensor.GetNextNumberInTheSequenceBackwards(realFilePath);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    private static Sensor CreateSensor()
+    {
+        return new Sensor();
+    }
+}
