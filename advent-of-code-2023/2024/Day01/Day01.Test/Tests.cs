3d759e85d3f1b54ede42995d60a6946ff32b9676
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-11-12 08:13:01 +0100

    chore: adapt data files to tests

---
+++ advent-of-code-2023/2024/Day01/Day01.Test/Tests.cs
@@ -0,0 +1,17 @@
+namespace Day01.Test;
+
+public class Tests
+{
+    private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day01.Src/Data/testData.txt";
+    private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day01.Src/Data/realData.txt";
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
