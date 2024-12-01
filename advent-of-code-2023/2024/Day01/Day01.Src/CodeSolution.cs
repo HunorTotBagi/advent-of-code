f35abd4fe0fba0dd5525d7a156cade7aa720a57f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-01 06:50:48 +0100

    feat: implement solution

--- advent-of-code-2023/2024/Day01/Day01.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day01/Day01.Src/CodeSolution.cs
@@ -2,4 +2,61 @@
 
 public class CodeSolution
 {
+    public (int[] arr1, int[] arr2) GetArraysFromFile()
+    {
+        var filePath = "C:\\Users\\htotbagi\\Downloads\\advent-of-code\\advent-of-code-2023\\2024\\Day01\\Day01.Src\\Data\\realData.txt";
+        var lines = File.ReadAllLines(filePath);
+
+        // Create the arrays
+        int[] leftNumbers = new int[lines.Length];
+        int[] rightNumbers = new int[lines.Length];
+
+        // Process each line
+        for (int i = 0; i < lines.Length; i++)
+        {
+            // Split the line into two parts
+            string[] parts = lines[i].Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Parse the numbers and assign to the arrays
+            leftNumbers[i] = int.Parse(parts[0]);
+            rightNumbers[i] = int.Parse(parts[1]);
+        }
+
+        return (leftNumbers, rightNumbers);
+    }
+
+    public List<int> Sort(int[] array)
+    {
+        return array.OrderByDescending(x => x).ToList();
+    }
+
+    public int[] CalculateDistance(int[] arr1, int[] arr2)
+    {
+        var result = new int[arr1.Length];
+
+        for (var i = 0; i < arr1.Length; i++)
+        {
+            result[i] = Math.Abs(arr1[i] - arr2[i]);
+        }
+
+        return result;
+    }
+
+    public int SummItUp(int[] arr)
+    {
+        return arr.Sum();
+    }
+
+    public object GetSimilarityScore(int[] arr1, int[] arr2)
+    {
+        var result = 0;
+
+        for (var i = 0; i < arr1.Length; i++)
+        {
+            var count = arr2.Count(x => x == arr1[i]);
+            result += arr1[i] * count;
+        }
+
+        return result;
+    }
 }
--- advent-of-code-2023/2024/Day01/Day01.Test/Tests.cs
+++ advent-of-code-2023/2024/Day01/Day01.Test/Tests.cs
@@ -1,4 +1,5 @@
 using Day01.Src;
+using FluentAssertions;
 
 namespace Day01.Test;
 
@@ -13,13 +14,90 @@ public class Tests
     public void Test1()
     {
         // Arrange
+        var (arr1, arr2) = _newCodeSolution.GetArraysFromFile();
+
+        var arr11 = new int[] {3, 4, 2, 1, 3, 3};
+        var arr22 = new int[] {4, 3, 5, 3, 9, 3 };
 
         // Act
 
         // Assert
+        arr1.Should().Equal(arr11);
+        arr2.Should().Equal(arr22);
     }
 
-    private static CodeSolution CreateCodeSolution()
+    [Fact]
+    public void Sorting()
+    {
+        // Arrange
+        var (arr1, arr2) = _newCodeSolution.GetArraysFromFile();
+
+        var res1 = arr1.OrderBy(x => x).ToList();
+        var res2 = arr2.OrderBy(x => x).ToList();
+
+        var expected1 = new int[] { 1, 2, 3, 3, 3, 4 };
+        var expected2 = new int[] { 3, 3, 3, 4, 5, 9 };
+
+        // Act
+
+        // Assert
+        res1.Should().Equal(expected1);
+        res2.Should().Equal(expected2);
+    }
+
+    [Fact]
+    public void CalcDistance()
+    {
+        // Arrange
+        var (arr1, arr2) = _newCodeSolution.GetArraysFromFile();
+
+        var res1 = arr1.OrderBy(x => x).ToArray();
+        var res2 = arr2.OrderBy(x => x).ToArray();
+
+        var checking = _newCodeSolution.CalculateDistance(res1, res2);
+
+        var expected = new int[] { 2, 1, 0, 1, 2, 5 };
+
+        // Act
+
+        // Assert
+        checking.Should().Equal(expected);
+    }
+
+    [Fact]
+    public void Finalnaswer()
+    {
+        // Arrange
+        var (arr1, arr2) = _newCodeSolution.GetArraysFromFile();
+
+        var res1 = arr1.OrderBy(x => x).ToArray();
+        var res2 = arr2.OrderBy(x => x).ToArray();
+
+        var checking = _newCodeSolution.CalculateDistance(res1, res2);
+
+        var final = _newCodeSolution.SummItUp(checking);
+
+        // Act
+
+        // Assert
+        final.Should().Be(11);
+    }
+
+    [Fact]
+    public void SecondPart()
+    {
+        // Arrange
+        var (arr1, arr2) = _newCodeSolution.GetArraysFromFile();
+
+        var result = _newCodeSolution.GetSimilarityScore(arr1, arr2);
+
+        // Act
+
+        // Assert
+        result.Should().Be(31);
+    }
+
+    public static CodeSolution CreateCodeSolution()
     {
         return new CodeSolution();
     }
