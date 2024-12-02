131c5a04a8304639e828713e2d955fe542da9305
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-02 07:33:44 +0100

    feat: solve problem 1 & 2

--- advent-of-code-2023/2024/Day02/Day02.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day02/Day02.Src/CodeSolution.cs
@@ -2,21 +2,79 @@
 
 public class CodeSolution
 {
-    public static int[] ReadFile(string filePath)
+    public static List<List<int>> ReadFile(string filePath)
     {
         var lines = File.ReadAllLines(filePath);
 
-        var array1 = new int[lines.Length];
-        var array2 = new int[lines.Length];
+        List<List<int>> listOfLists = new List<List<int>>();
 
-        for (var i = 0; i < lines.Length; i++)
+        foreach (string line in lines)
         {
-            var parts = lines[i].Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries);
-
-            array1[i] = int.Parse(parts[0]);
-            array2[i] = int.Parse(parts[1]);
+            List<int> row = line.Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            listOfLists.Add(row);
         }
 
-        return array1;
+        return listOfLists;
+    }
+
+    public static bool ReportIsSafe(List<int> report)
+    {
+        var increasing = true;
+        var decreasing = true;
+
+        for (var i = 0; i < report.Count - 1; i++)
+        {
+            if (report[i] >= report[i + 1])
+                increasing = false;
+
+            if (report[i] <= report[i + 1])
+                decreasing = false;
+
+            var difference = Math.Abs(report[i] - report[i + 1]);
+            if (difference < 1 || difference > 3)
+                return false;
+        }
+
+        return increasing || decreasing;
+    }
+
+    public static int TotalSafeReports(List<List<int>> input)
+    {
+        var counter = 0;
+
+        foreach (var list in input)
+        {
+            if (ReportIsSafe(list))
+                counter++;
+        }
+
+        return counter;
+    }
+
+    public static bool IsSafeWithOneRemoval(List<int> report)
+    {
+        for (var i = 0; i < report.Count; i++)
+        {
+            var modifiedReport = report.Where((val, index) => index != i).ToList();
+            if (ReportIsSafe(modifiedReport))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int TotalSafeReportsWithOneRemoval(List<List<int>> input)
+    {
+        var counter = 0;
+        foreach (var list in input)
+        {
+            if (IsSafeWithOneRemoval(list))
+                counter++;
+        }
+
+        return counter;
     }
 }
--- advent-of-code-2023/2024/Day02/Day02.Test/Tests.cs
+++ advent-of-code-2023/2024/Day02/Day02.Test/Tests.cs
@@ -1,4 +1,5 @@
 using Day02.Src;
+using FluentAssertions;
 
 namespace Day02.Test
 {
@@ -8,25 +9,122 @@ namespace Day02.Test
         private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day02.Src/Data/realData.txt";
 
         [Fact]
-        public void Test1()
+        public void Reads_The_Data()
         {
             // Arrange
-            CodeSolution.ReadFile(_testData);
+            List<List<int>> listOfLists =
+            [
+                new() { 7, 6, 4, 2, 1 },
+                new() { 1, 2, 7, 8, 9 },
+                new() { 9, 7, 6, 2, 1 },
+                new() { 1, 3, 2, 4, 5 },
+                new() { 8, 6, 4, 4, 1 },
+                new() { 1, 3, 6, 7, 9 }
+            ];
 
             // Act
+            var result = CodeSolution.ReadFile(_testData);
 
             // Assert
+            result.Should().BeEquivalentTo(listOfLists);
         }
 
         [Fact]
-        public void Test2()
+        public void Report_Is_Safe()
         {
             // Arrange
-            CodeSolution.ReadFile(_testData);
+            var matrix = CodeSolution.ReadFile(_testData);
 
             // Act
+            var row0 = CodeSolution.ReportIsSafe(matrix[0]);
+            var row1 = CodeSolution.ReportIsSafe(matrix[1]);
+            var row2 = CodeSolution.ReportIsSafe(matrix[2]);
+            var row3 = CodeSolution.ReportIsSafe(matrix[3]);
+            var row4 = CodeSolution.ReportIsSafe(matrix[4]);
+            var row5 = CodeSolution.ReportIsSafe(matrix[5]);
+
+            //Assert
+            row0.Should().Be(true);
+            row1.Should().Be(false);
+            row2.Should().Be(false);
+            row3.Should().Be(false);
+            row4.Should().Be(false);
+            row5.Should().Be(true);
+        }
+
+        [Fact]
+        public void Count_Safe_Reports_For_Test_Data()
+        {
+            // Arrange
+            var matrix = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.TotalSafeReports(matrix);
+
+            //Assert
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Count_Safe_Reports_For_Real_Data()
+        {
+            // Arrange
+            var matrix = CodeSolution.ReadFile(_realData);
+
+            // Act
+            var result = CodeSolution.TotalSafeReports(matrix);
 
             // Assert
+            result.Should().Be(383);
+        }
+
+        [Fact]
+        public void IsSafeWithRemoval()
+        {
+            // Arrange
+            var matrix = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var row0 = CodeSolution.IsSafeWithOneRemoval(matrix[0]);
+            var row1 = CodeSolution.IsSafeWithOneRemoval(matrix[1]);
+            var row2 = CodeSolution.IsSafeWithOneRemoval(matrix[2]);
+            var row3 = CodeSolution.IsSafeWithOneRemoval(matrix[3]);
+            var row4 = CodeSolution.IsSafeWithOneRemoval(matrix[4]);
+            var row5 = CodeSolution.IsSafeWithOneRemoval(matrix[5]);
+
+            //Assert
+            row0.Should().Be(true);
+            row1.Should().Be(false);
+            row2.Should().Be(false);
+            row3.Should().Be(true);
+            row4.Should().Be(true);
+            row5.Should().Be(true);
+        }
+
+        [Fact]
+        public void Count_Safe_Reports_With_One_Removal_For_Test_Data()
+        {
+            // Arrange
+            var matrix = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.TotalSafeReportsWithOneRemoval(matrix);
+
+            //Assert
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void Count_Safe_Reports_With_One_Removal_For_Real_Data()
+        {
+            // Arrange
+            var matrix = CodeSolution.ReadFile(_realData);
+
+            // Act
+            var result = CodeSolution.TotalSafeReportsWithOneRemoval(matrix);
+
+            // Assert
+            result.Should().Be(436);
         }
     }
 }
