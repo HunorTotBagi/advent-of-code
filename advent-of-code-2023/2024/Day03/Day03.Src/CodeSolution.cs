0340d8222be8da6ed6865f21c4ddd13cd6f80a49
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-02 09:37:05 +0100

    feat: prepare file read in

--- advent-of-code-2023/2024/Day03/Day03.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day03/Day03.Src/CodeSolution.cs
@@ -2,4 +2,10 @@
 
 public class CodeSolution
 {
+    public static List<int> ReadFile(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+
+        return new List<int>();
+    }
 }
