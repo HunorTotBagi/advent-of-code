eb2a35b4b113b228b4256da418a1ce63800efe6c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-03 13:18:28 +0100

    feat: add more code for starting template

--- advent-of-code-2023/2024/Day04/Day04.Test/Tests.cs
+++ advent-of-code-2023/2024/Day04/Day04.Test/Tests.cs
@@ -1,11 +1,24 @@
+using Day04.Src;
+using FluentAssertions;
+
 namespace Day04.Test
 {
     public class Tests
     {
+        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day04.Src/Data/testData.txt";
+        private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day04.Src/Data/realData.txt";
+
         [Fact]
         public void Test1()
         {
+            // Arrange
+            var expected = new List<string> {"a", "b"};
 
+            // Act
+            var result = CodeSolution.ReadFile(_testData);
+
+            // Assert
+            result.Should().Equal(expected);
         }
     }
 }
