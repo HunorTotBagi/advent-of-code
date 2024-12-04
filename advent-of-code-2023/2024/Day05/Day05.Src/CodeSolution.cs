a7871551c1c06465d7e62a7a9b2e2d3ba7564c66
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-04 07:43:59 +0100

    feat: create initial setup

---
+++ advent-of-code-2023/2024/Day05/Day05.Src/CodeSolution.cs
@@ -0,0 +1,9 @@
+﻿namespace Day05.Src;
+
+public class CodeSolution
+{
+    public static void ReadFile(string filePath)
+    {
+
+    }
+}
---
+++ advent-of-code-2023/2024/Day05/Day05.Test/Tests.cs
@@ -0,0 +1,21 @@
+using Day05.Src;
+using FluentAssertions;
+
+namespace Day05.Test
+{
+    public class Tests
+    {
+        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day05.Src/Data/testData.txt";
+        private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day05.Src/Data/realData.txt";
+
+        [Fact]
+        public void Reads_Data()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
