2f85b1fc1bb52368bfafc6c848cb6b1c26486ff0
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-05 07:40:56 +0100

    feat: solve problem 1 & 2

--- advent-of-code-2023/2024/Day05/Day05.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day05/Day05.Src/CodeSolution.cs
@@ -2,8 +2,141 @@
 
 public class CodeSolution
 {
-    public static void ReadFile(string filePath)
+    public static List<List<int>> ReadFileRules(string filePath)
     {
+        var lines = File.ReadAllLines(filePath);
 
+        var listOfNumber = new List<List<int>>();
+
+        foreach (var line in lines)
+        {
+            var numbers = new List<int>();
+            var parts = line.Split("|");
+
+            numbers.Add(int.Parse(parts[0]));
+            numbers.Add(int.Parse(parts[1]));
+
+            listOfNumber.Add(numbers);
+        }
+
+        return listOfNumber;
+    }
+
+    public static List<List<int>> ReadFileRows(string filePath)
+    {
+        var lines = File.ReadAllLines((filePath));
+
+        var listOfNumbers = new List<List<int>>();
+
+        foreach (var line in lines)
+        {
+            var numbers = new List<int>();
+            var parts = line.Split(",");
+
+            foreach (var part in parts)
+            {
+                numbers.Add(int.Parse(part));
+            }
+
+            listOfNumbers.Add(numbers);
+        }
+        return listOfNumbers;
+    }
+
+    public static bool IsPairInRules(int first, int second, List<List<int>> rules)
+    {
+        for (var i = 0; i < rules.Count; i++)
+        {
+            if (rules[i][0] == first && rules[i][1] == second)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static bool IsRowCorrect(List<int> row, List<List<int>> rules)
+    {
+        for (var i = 0; i < row.Count - 1; i++)
+        {
+            for (var j = i + 1; j < row.Count; j++)
+            {
+                if (!IsPairInRules(row[i], row[j], rules))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static int CountCorrectRows(List<List<int>> rows, List<List<int>> rules)
+    {
+        var sum = 0;
+
+        foreach (var row in rows)
+        {
+            if (IsRowCorrect(row, rules))
+            {
+                var index = row.Count / 2;
+                sum += row[index];
+            }
+        }
+        return sum;
+    }
+
+    public static List<int> SwapWhileNotFound(List<int> row, List<List<int>> rules)
+    { 
+        while (!IsRowCorrect(row, rules))
+        {
+            for (var i = 0; i < row.Count - 1; i++)
+            {
+                for (var j = i + 1; j < row.Count; j++)
+                {
+                    var boolean = IsPairInRules(row[i], row[j], rules);
+                    if (!boolean)
+                    {
+                        (row[i], row[j]) = (row[j], row[i]);
+                    }
+                }
+            }
+        }
+
+        return row;
+    }
+
+    public static int CalculateFalseSwapped(List<int> row, List<List<int>> rules)
+    {
+        while (!IsRowCorrect(row, rules))
+        {
+            for (var i = 0; i < row.Count - 1; i++)
+            {
+                for (var j = i + 1; j < row.Count; j++)
+                {
+                    var boolean = IsPairInRules(row[i], row[j], rules);
+                    if (!boolean)
+                    {
+                        (row[i], row[j]) = (row[j], row[i]);
+                    }
+                }
+            }
+        }
+
+        var index = row.Count / 2;
+        return row[index];
+    }
+
+    public static int CountFalseSwappedRows(List<List<int>> rows, List<List<int>> rules)
+    {
+        var sum = 0;
+
+        foreach (var row in rows)
+        {
+            if (!IsRowCorrect(row, rules))
+            {
+                sum += CalculateFalseSwapped(row, rules);
+            }
+        }
+
+        return sum;
     }
 }
--- advent-of-code-2023/2024/Day05/Day05.Test/Tests.cs
+++ advent-of-code-2023/2024/Day05/Day05.Test/Tests.cs
@@ -5,17 +5,151 @@ namespace Day05.Test
 {
     public class Tests
     {
-        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day05.Src/Data/testData.txt";
-        private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day05.Src/Data/realData.txt";
+        private readonly string _testData0 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day05.Src/testData0.txt";
+        private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day05.Src/testData1.txt";
 
         [Fact]
-        public void Reads_Data()
+        public void Read_Rules()
         {
             // Arrange
+            var expected = new List<List<int>>
+            {
+                new() { 47, 53 },
+                new() { 97, 13 },
+                new() { 97, 61 },
+                new() { 97, 47 },
+                new() { 75, 29 },
+                new() { 61, 13 },
+                new() { 75, 53 },
+                new() { 29, 13 },
+                new() { 97, 29 },
+                new() { 53, 29 },
+                new() { 61, 53 },
+                new() { 97, 53 },
+                new() { 61, 29 },
+                new() { 47, 13 },
+                new() { 75, 47 },
+                new() { 97, 75 },
+                new() { 47, 61 },
+                new() { 75, 61 },
+                new() { 47, 29 },
+                new() { 75, 13 },
+                new() { 53, 13 }
+            };
 
             // Act
+            var rules = CodeSolution.ReadFileRules(_testData0);
 
             // Assert
+            rules.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Read_Rows()
+        {
+            // Arrange
+            var expected = new List<List<int>>
+            {
+                new() { 75, 47, 61, 53, 29 },
+                new() { 97, 61, 53, 29, 13 },
+                new() { 75, 29, 13 },
+                new() { 75, 97, 47, 61, 53 },
+                new() { 61, 13, 29 },
+                new() { 97, 13, 75, 29, 47 }
+            };
+
+            // Act
+            var rows = CodeSolution.ReadFileRows(_testData1);
+
+            // Assert
+            rows.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Is_Targeted_Pair_In_Rules()
+        {
+            // Arrange
+            var rules = CodeSolution.ReadFileRules(_testData0);
+
+            // Act
+            var result = CodeSolution.IsPairInRules(75,53, rules);
+
+            // Assert
+            result.Should().Be(true);
+        }
+
+        [Fact]
+        public void Is_Targeted_Row_Correct_Or_Not()
+        {
+            // Arrange
+            var rules = CodeSolution.ReadFileRules(_testData0);
+            var rows = CodeSolution.ReadFileRows(_testData1);
+
+            // Act
+            var result0 = CodeSolution.IsRowCorrect(rows[0], rules);
+            var result1 = CodeSolution.IsRowCorrect(rows[1], rules);
+            var result2 = CodeSolution.IsRowCorrect(rows[2], rules);
+            var result3 = CodeSolution.IsRowCorrect(rows[3], rules);
+            var result4 = CodeSolution.IsRowCorrect(rows[4], rules);
+            var result5 = CodeSolution.IsRowCorrect(rows[5], rules);
+
+            // Assert
+            result0.Should().Be(true);
+            result1.Should().Be(true);
+            result2.Should().Be(true);
+            result3.Should().Be(false);
+            result4.Should().Be(false);
+            result5.Should().Be(false);
+        }
+
+        [Fact]
+        public void Count_All_Middle_Elements_Of_Correct_Rows()
+        {
+            // Arrange
+            var rules = CodeSolution.ReadFileRules(_testData0);
+            var rows = CodeSolution.ReadFileRows(_testData1);
+
+            // Act
+            var result = CodeSolution.CountCorrectRows(rows, rules);
+
+            // Assert
+            result.Should().Be(143);
+        }
+
+        [Fact]
+        public void Swap_False_Rows_Until_Correct()
+        {
+            // Arrange
+            var rules = CodeSolution.ReadFileRules(_testData0);
+            var rows = CodeSolution.ReadFileRows(_testData1);
+
+            var expected3 = new List<int> { 97, 75, 47, 61, 53 };
+            var expected4 = new List<int> { 61, 29, 13 };
+            var expected5 = new List<int> { 97, 75, 47, 29, 13 };
+
+            // Act
+            var result3 = CodeSolution.SwapWhileNotFound(rows[3], rules);
+            var result4 = CodeSolution.SwapWhileNotFound(rows[4], rules);
+            var result5 = CodeSolution.SwapWhileNotFound(rows[5], rules);
+
+            // Assert
+            result3.Should().Equal(expected3);
+            result4.Should().Equal(expected4);
+            result5.Should().Equal(expected5);
+        }
+
+        [Fact]
+        public void Count_All_Middle_Elements_Of_False_Swapped_Rows()
+        {
+            // Arrange
+            var rules = CodeSolution.ReadFileRules(_testData0);
+            var data = CodeSolution.ReadFileRows(_testData1);
+
+            // Act
+            var result0 = CodeSolution.CountFalseSwappedRows(data, rules);
+
+            // Assert
+            result0.Should().Be(123);
         }
     }
 }
