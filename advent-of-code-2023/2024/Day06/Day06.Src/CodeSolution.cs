277897c5d00fab60153db0f0c58bf2cc56fc13fa
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-06 14:39:35 +0100

    feat: solve problem 1 & 2

--- advent-of-code-2023/2024/Day06/Day06.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day06/Day06.Src/CodeSolution.cs
@@ -2,13 +2,115 @@
 
 public class CodeSolution
 {
-    public static int ReadFile(string filePath)
+    public static List<List<char>> ReadFile(string filePath)
     {
         var lines = File.ReadAllLines(filePath);
 
+        var listOfChars = new List<List<char>>();
+
         foreach (var line in lines)
         {
+            listOfChars.Add(new List<char>(line));
         }
-        return 5;
+
+        return listOfChars;
+    }
+
+    public static (int first, int second) FindGuardPosition(List<List<char>> input)
+    {
+        for (var i = 0; i < input.Count; i++)
+        {
+            for (var j = 0; j < input[0].Count; j++)
+            {
+                if (input[i][j] == '^' || input[i][j] == '<' || input[i][j] == 'v' || input[i][j] == '>')
+                {
+                    return (i, j);
+                }
+            }
+        }
+        return (-1, -1);
+    }
+
+    public static int Count(int i, int j, List<List<char>> input)
+    {
+        var directionRow = -1;
+        var directionColumn = 0;
+
+        var seenPositions = new HashSet<Tuple<int, int>>();
+
+        while (true)
+        {
+            seenPositions.Add(Tuple.Create(i, j));
+            if (i + directionRow < 0 || i + directionRow >= input.Count || j + directionColumn < 0 || j + directionColumn >= input[0].Count)
+                break;
+
+            if (input[i + directionRow][j + directionColumn] == '#')
+            {
+                var temp = directionColumn;
+                directionColumn = -directionRow;
+                directionRow = temp;
+            }
+            else
+            {
+                i += directionRow;
+                j += directionColumn;
+            }
+        }
+        return seenPositions.Count;
+    }
+
+    public static bool GuardIsLooping(int i, int j, List<List<char>> input)
+    {
+        var directionRow = -1;
+        var directionColumn = 0;
+
+        var seenPositions = new HashSet<Tuple<int, int, int, int>>();
+
+        while (true)
+        {
+            seenPositions.Add(Tuple.Create(i, j, directionRow, directionColumn));
+            if (i + directionRow < 0 || i + directionRow >= input.Count || j + directionColumn < 0 || j + directionColumn >= input[0].Count)
+                break;
+
+            if (input[i + directionRow][j + directionColumn] == '#')
+            {
+                var temp = directionColumn;
+                directionColumn = -directionRow;
+                directionRow = temp;
+            }
+            else
+            {
+                i += directionRow;
+                j += directionColumn;
+            }
+            if (seenPositions.Contains(Tuple.Create(i, j, directionRow, directionColumn)))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static int CountLoops(int x, int y, List<List<char>> input)
+    {
+        var count = 0;
+        var rows = input.Count;
+        var cols = input[0].Count;
+        
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < cols; j++)
+            {
+                if (input[i][j] != '.') continue;
+                input[i][j] = '#';
+
+                if (GuardIsLooping(x, y, input))
+                    count++;
+
+                input[i][j] = '.';
+            }
+        }
+        return count;
     }
 }
--- advent-of-code-2023/2024/Day06/Day06.Test/Tests.cs
+++ advent-of-code-2023/2024/Day06/Day06.Test/Tests.cs
@@ -3,19 +3,75 @@ using FluentAssertions;
 
 namespace Day06.Test
 {
-    public class UnitTest1
+    public class Tests
     {
-        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day06.Src/testData0.txt";
+        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day06.Src/testData.txt";
 
         [Fact]
         public void Test1()
         {
             // Arrange
+            var expected = new List<List<char>>
+            {
+                new List<char> { '.', '.', '.', '.', '#', '.', '.', '.', '.', '.' },
+                new List<char> { '.', '.', '.', '.', '.', '.', '.', '.', '.', '#' },
+                new List<char> { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                new List<char> { '.', '.', '#', '.', '.', '.', '.', '.', '.', '.' },
+                new List<char> { '.', '.', '.', '.', '.', '.', '#', '.', '.', '.' },
+                new List<char> { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                new List<char> { '.', '#', '.', '.', '^', '.', '.', '.', '.', '.' },
+                new List<char> { '.', '.', '.', '.', '.', '.', '.', '.', '#', '.' },
+                new List<char> { '#', '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                new List<char> { '.', '.', '.', '.', '.', '.', '#', '.', '.', '.' }
+            };
+
+            // Act
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Assert
+            data.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void FindGuard()
+        {
+            // Arrange
             var data = CodeSolution.ReadFile(_testData);
 
             // Act
+            var (firstIndex, secondIndex) = CodeSolution.FindGuardPosition(data);
 
             // Assert
+            firstIndex.Should().Be(6);
+            secondIndex.Should().Be(4);
+        }
+
+        [Fact]
+        public void Count()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var (firstIndex, secondIndex) = CodeSolution.FindGuardPosition(data);
+            var result = CodeSolution.Count(firstIndex, secondIndex, data);
+
+            // Assert
+            result.Should().Be(41);
+        }
+
+        [Fact]
+        public void Count1()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var (firstIndex, secondIndex) = CodeSolution.FindGuardPosition(data);
+            var result = CodeSolution.CountLoops(firstIndex, secondIndex, data);
+
+            // Assert
+            result.Should().Be(6);
         }
     }
 }
