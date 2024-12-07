4a3e36a24dbf5d8266a12897698a035f99c14c6c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-07 07:22:15 +0100

    feat: solve problem 1 & 2

--- advent-of-code-2023/2024/Day07/Day07.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day07/Day07.Src/CodeSolution.cs
@@ -2,15 +2,141 @@
 
 public class CodeSolution
 {
-    public static int ReadFile(string filePath)
+    public static (List<long> first, List<List<long>> second) ReadFile(string filePath)
     {
         var lines = File.ReadAllLines(filePath);
 
+        var number = new List<long>();
+        var data = new List<List<long>>();
+
         foreach (var line in lines)
         {
+            var parts = line.Split(": ");
+            number.Add(long.Parse(parts[0]));
 
+            var newList = new List<long>();
+            var newParts = parts[1].Split(" ");
+            foreach (var element in newParts)
+            {
+                newList.Add(long.Parse(element));
+            }
+            data.Add(newList);
         }
 
-        return 5;
+        return (number, data);
+    }
+
+    public static List<string> GenerateCombinations(long n)
+    {
+        var result = new List<string>();
+        GenerateCombinationsHelper("", n, result);
+        return result;
+    }
+
+    public static void GenerateCombinationsHelper(string current, long remaining, List<string> result)
+    {
+        if (remaining == 0)
+        {
+            result.Add(current);
+            return;
+        }
+
+        GenerateCombinationsHelper(current + "+", remaining - 1, result);
+        GenerateCombinationsHelper(current + "*", remaining - 1, result);
+    }
+
+    public static bool IsValid(long number, List<long> input)
+    {
+        var length = input.Count - 1;
+        var symbolCombinations = GenerateCombinations(length);
+
+        foreach (var combination in symbolCombinations)
+        {
+            var answer = input[0];
+            for (var i = 0; i < combination.Length; i++)
+            {
+                if (combination[i] == '+')
+                    answer += input[i + 1];
+
+                if (combination[i] == '*')
+                    answer *= input[i + 1];
+            }
+
+            if (answer == number)
+                return true;
+        }
+        return false;
+    }
+
+    public static long Calc(List<long> numbers, List<List<long>> inputs)
+    {
+        long result = 0;
+        for (var i = 0; i < numbers.Count; i++)
+        {
+            if (IsValid(numbers[i], inputs[i]))
+                result += numbers[i];
+        }
+        return result;
+    }
+
+    public static List<string> GenerateCombinationsConcat(long n)
+    {
+        var result = new List<string>();
+        GenerateCombinationsHelperConcat("", n, result);
+        return result;
+    }
+
+    public static void GenerateCombinationsHelperConcat(string current, long remaining, List<string> result)
+    {
+        if (remaining == 0)
+        {
+            result.Add(current);
+            return;
+        }
+
+        GenerateCombinationsHelperConcat(current + "+", remaining - 1, result);
+        GenerateCombinationsHelperConcat(current + "*", remaining - 1, result);
+        GenerateCombinationsHelperConcat(current + "|", remaining - 1, result);
+    }
+
+    public static bool IsValidConcatenation(long number, List<long> input)
+    {
+        var length = input.Count - 1;
+        var symbolCombinations = GenerateCombinationsConcat(length);
+
+        foreach (var combination in symbolCombinations)
+        {
+            var answer = input[0];
+            for (var i = 0; i < combination.Length; i++)
+            {
+                if (combination[i] == '+')
+                    answer += input[i + 1];
+
+                if (combination[i] == '*')
+                    answer *= input[i + 1];
+                if (combination[i] == '|')
+                {
+                    long numberLength = input[i + 1].ToString().Length;
+                    answer *= (long)Math.Pow(10, numberLength);
+                    answer += input[i + 1];
+                }
+
+            }
+
+            if (answer == number)
+                return true;
+        }
+        return false;
+    }
+
+    public static long CalculateConcatenation(List<long> numbers, List<List<long>> inputs)
+    {
+        long result = 0;
+        for (var i = 0; i < numbers.Count; i++)
+        {
+            if (IsValidConcatenation(numbers[i], inputs[i]))
+                result += numbers[i];
+        }
+        return result;
     }
 }
--- advent-of-code-2023/2024/Day07/Day07.Test/Tests.cs
+++ advent-of-code-2023/2024/Day07/Day07.Test/Tests.cs
@@ -1,4 +1,5 @@
 using Day07.Src;
+using FluentAssertions;
 
 namespace Day07.Test
 {
@@ -7,14 +8,112 @@ namespace Day07.Test
         private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day07.Src/testData.txt";
 
         [Fact]
-        public void Test1()
+        public void Read_Data()
         {
             // Arrange
-            var data = CodeSolution.ReadFile(_testData);
+            var leftNumbers = new List<long> { 190, 3267, 83, 156, 7290,161011, 192, 21037, 292 };
+
+            var rightNumbers = new List<List<long>>
+            {
+                new() { 10, 19 }, new() { 81, 40, 27 }, new() { 17, 5 },
+                new() { 15, 6 }, new() { 6, 8, 6, 15 }, new() { 16, 10, 13 },
+                new() { 17, 8, 14 }, new() { 9, 7, 18, 13 }, new() { 11, 6, 16, 20 }
+            };
 
             // Act
+            var (number, all) = CodeSolution.ReadFile(_testData);
 
             // Assert
+            number.Should().BeEquivalentTo(leftNumbers);
+            all.Should().BeEquivalentTo(rightNumbers);
+        }
+
+        [Theory]
+        [InlineData(1, new[] { "+", "*" })]
+        [InlineData(2, new[] { "++", "+*", "*+", "**" })]
+        public void Generates_Correct_Symbol_Combinations(int input, string[] expected)
+        {
+            // Arrange
+            var expectedList = new List<string>(expected);
+
+            // Act
+            var result = CodeSolution.GenerateCombinations(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedList);
+        }
+
+        [Theory]
+        [InlineData(190, new long[] { 10, 19 }, true)]
+        [InlineData(3267, new long[] { 81, 40, 27 }, true)]
+        [InlineData(83, new long[] { 17, 5 }, false)]
+        public void Check_Is_Row_Valid(long target, long[] elements, bool expected)
+        {
+            // Arrange
+            var elementsList = new List<long>(elements);
+
+            // Act
+            var result = CodeSolution.IsValid(target, elementsList);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Sum_Up_Valid_Rows()
+        {
+            // Arrange
+            var (targetNumbers, elements) = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.Calc(targetNumbers, elements);
+
+            // Assert
+            result.Should().Be(3749);
+        }
+
+        [Theory]
+        [InlineData(1, new[] { "+", "*", "|" })]
+        [InlineData(2, new[] { "++", "+*", "+|", "*+", "**", "*|", "|+", "|*", "||" })]
+        public void Generates_Correct_Symbol_Combinations_Concatenation(int input, string[] expected)
+        {
+            // Arrange
+            var expectedList = new List<string>(expected);
+
+            // Act
+            var result = CodeSolution.GenerateCombinationsConcat(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedList);
+        }
+
+        [Theory]
+        [InlineData(156, new long[] { 15, 6 }, true)]
+        [InlineData(7290, new long[] { 6, 8, 6, 15 }, true)]
+        [InlineData(192, new long[] { 17, 8, 14 }, true)]
+        public void Check_Is_Row_Valid_With_Concatenation_Symbol(long target, long[] elements, bool expected)
+        {
+            // Arrange
+            var elementsList = new List<long>(elements);
+
+            // Act
+            var result = CodeSolution.IsValidConcatenation(target, elementsList);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Sum_Up_Valid_Rows_With_Concatenation()
+        {
+            // Arrange
+            var (targetNumbers, elements) = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.CalculateConcatenation(targetNumbers, elements);
+
+            // Assert
+            result.Should().Be(11387);
         }
     }
 }
