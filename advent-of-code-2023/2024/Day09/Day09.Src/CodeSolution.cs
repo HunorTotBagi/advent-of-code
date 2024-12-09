9ca8fd61f340957eba7060a9ccd1a389fecc02da
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-09 08:35:17 +0100

    feat: solve part 1

--- advent-of-code-2023/2024/Day09/Day09.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day09/Day09.Src/CodeSolution.cs
@@ -1,18 +1,106 @@
-﻿namespace Day09.Src;
+﻿
+namespace Day09.Src;
 
 public class CodeSolution
 {
-    public static int ReadFile(string filePath)
+    public static List<ulong> ReadFile(string filePath)
     {
         var lines = File.ReadAllLines(filePath);
-
-        var grid1 = new List<List<int>>();
-        var grid2 = new List<List<char>>();
+        var grid1 = new List<ulong>();
 
         foreach (var line in lines)
         {
-
+            foreach (var c in line)
+            {
+                if (char.IsDigit(c))
+                {
+                    grid1.Add(ulong.Parse(c.ToString()));
+                }
+            }
         }
+        return grid1;
+    }
+
+    public static List<string> ConvertToDots(List<ulong> input)
+    {
+        var result = new List<string>();
+        var counter = 0;
+
+        for (var i = 0; i < input.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                for (ulong j = 0; j < input[i]; j++)
+                    result.Add(counter.ToString());
+                counter++;
+            }
+            else
+            {
+                for (ulong j = 0; j < input[i]; j++)
+                    result.Add(".");
+            }
+        }
+        return result;
+    }
+
+    public static ulong CountTheDots(List<ulong> input)
+    {
+        var asd = ConvertToDots(input);
+        ulong result = 0;
+        foreach (var chara in asd)
+        {
+            if (chara == ".")
+                result++;
+        }
+        return result;
+    }
+
+    public static List<string> SwapDots(List<ulong> input)
+    {
+        var myString = ConvertToDots(input);
+
+        for (var j = myString.Count - 1; j > 0; j--)
+        {
+            if (myString[j] != ".")
+            {
+                for (var i = 0; i < j; i++)
+                {
+                    if (myString[i] == ".")
+                    {
+                        myString[i] = myString[j];
+                        myString[j] = ".";
+                        break;
+                    }
+                }
+                
+            }
+        }
+
+        return myString;
+    }
+
+    public static ulong CalculatePartOne(List<ulong> input)
+    {
+        ulong result = 0;
+        var counter = 0;
+
+        var myString = SwapDots(input);
+
+        foreach (var character in myString)
+        {
+            if (character == ".")
+            {
+                break;
+            }
+            result += (ulong)(counter * int.Parse(character));
+            counter++;
+        }
+
+        return result;
+    }
+
+    public static int CalculatePartTwo(int data)
+    {
         return 5;
     }
 }
--- advent-of-code-2023/2024/Day09/Day09.Test/Tests.cs
+++ advent-of-code-2023/2024/Day09/Day09.Test/Tests.cs
@@ -6,18 +6,86 @@ namespace Day09.Test
     public class Tests
     {
         private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/testData.txt";
+        private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/realData.txt";
 
         [Fact]
-        public void Test1()
+        public void ReadFile()
         {
             // Arrange
-
+            var expected = new List<ulong> { 2, 3, 3, 3, 1, 3, 3, 1, 2, 1, 4, 1, 4, 1, 3, 1, 4, 0, 2 };
 
             // Act
             var data = CodeSolution.ReadFile(_testData);
 
             // Assert
-            data.Should().Be(5);
+            data.Should().Equal(expected);
         }
+
+        [Fact]
+        public void ConvertToDots()
+        {
+            // Arrange
+            var expected = "00...111...2...333.44.5555.6666.777.888899";
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.ConvertToDots(data);
+
+            // Assert
+            //result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CountDOts()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.CountTheDots(data);
+
+            // Assert
+            result.Should().Be(14);
+        }
+
+        [Fact]
+        public void SwapDots()
+        {
+            // Arrange
+            var expected = "0099811188827773336446555566..............";
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.SwapDots(data);
+
+            // Assert
+            //result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void CalculatePartOne()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_realData);
+
+            // Act
+            var result = CodeSolution.CalculatePartOne(data);
+
+            // Assert
+            result.Should().Be(1928);
+        }
+
+        //[Fact]
+        //public void CalculatePartTwo()
+        //{
+        //    // Arrange
+        //    var data = CodeSolution.ReadFile(_testData);
+
+        //    // Act
+        //    var result = CodeSolution.CalculatePartTwo(data);
+
+        //    // Assert
+        //    result.Should().Be(5);
+        //}
     }
 }
