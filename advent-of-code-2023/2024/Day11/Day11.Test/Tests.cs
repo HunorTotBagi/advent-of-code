22795c78126bef700e91913271ea55b3832376c5
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-11 08:17:42 +0100

    feat: solve part 1 & 2

--- advent-of-code-2023/2024/Day11/Day11.Test/Tests.cs
+++ advent-of-code-2023/2024/Day11/Day11.Test/Tests.cs
@@ -5,22 +5,65 @@ namespace Day11.Test
 {
     public class Tests
     {
-        private readonly string _testData0 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/testData0.txt";
-        private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/testData1.txt";
-        private readonly string _testData2 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day09.Src/testData2.txt";
-
+        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day11.Src/testData.txt";
 
         [Fact]
         public void ReadFile()
         {
             // Arrange
-            var result = 5;
+            var expected = new List<int> { 125, 17 };
 
             // Act
-            var data = CodeSolution.ReadFile(_testData0);
+            var result = CodeSolution.ReadFile(_testData);
 
             // Assert
-            data.Should().Be(result);
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData(1235, 12,35)]
+        [InlineData(812135, 812,135)]
+        public void SplitEvenNumber(ulong input, ulong firstPartExpected, ulong secondPartExpected)
+        {
+            // Arrange
+
+            // Act
+            var (first, second) = CodeSolution.SplitEvenNumber(input);
+
+            // Assert
+            first.Should().Be(firstPartExpected);
+            second.Should().Be(secondPartExpected);
+        }
+
+        [Theory]
+        [InlineData("0000123", 123)]
+        [InlineData("07104", 7104)]
+        [InlineData("000", 0)]
+        [InlineData("0", 0)]
+        public void RemoveLeadingZeros(string input, ulong expected)
+        {
+            // Arrange
+
+            // Act
+            var result = CodeSolution.RemoveLeadingZeros(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(6, 22)]
+        [InlineData(25, 55312)]
+        public void Calc(ulong numberOfBlinks, ulong expected)
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.CalculateStoneCountAfterBlinks(data, numberOfBlinks);
+
+            // Assert
+            result.Should().Be(expected);
         }
     }
 }
