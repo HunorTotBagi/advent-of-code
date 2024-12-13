60f73da9429bf7d9237f31dc1c39a517f7f568bd
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-13 06:55:39 +0100

    feat: unfinished part 1

--- advent-of-code-2023/2024/Day12/Day12.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day12/Day12.Src/CodeSolution.cs
@@ -2,17 +2,201 @@
 
 public class CodeSolution
 {
-    public static int ReadFile(string filePath)
+    public static List<List<char>> ReadFile(string filePath)
     {
         var lines = File.ReadAllLines(filePath);
 
-        var result1 = new List<List<int>>();
-        var result2 = new List<List<char>>();
+        var result = new List<List<char>>();
 
         foreach (var line in lines)
         {
-
+            var holder = new List<char>();
+            foreach (var ch in line)
+            {
+                holder.Add(ch);
+            }
+            result.Add(holder);
         }
-        return 5;
+        return result;
+    }
+
+    public static HashSet<char> GetUniqueCharacter(List<List<char>> input)
+    {
+        var unique = new HashSet<char>();
+        for (var i = 0; i < input.Count; i++)
+        {
+            for (var j = 0; j < input[0].Count; j++)
+            {
+                if (!unique.Contains(input[i][j]))
+                {
+                    unique.Add(input[i][j]);
+                }
+            }
+        }
+
+        return unique;
+    }
+
+    public static int GetArea(char flower, List<List<char>> input)
+    {
+        var result = 0;
+        for (var i = 0; i < input.Count; i++)
+        {
+            for (var j = 0; j < input[0].Count; j++)
+            {
+                if (input[i][j] == flower)
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+    public static int GetPerimeter(char letter, List<List<char>> grid)
+    {
+        int rows = grid.Count;
+        int cols = grid[0].Count;
+        bool[,] visited = new bool[rows, cols];
+
+        // Directions: up, down, left, right
+        int[,] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+        bool IsValid(int x, int y)
+        {
+            return x >= 0 && x < rows && y >= 0 && y < cols;
+        }
+
+        int FloodFillPerimeter(int x, int y)
+        {
+            Stack<(int, int)> stack = new Stack<(int, int)>();
+            stack.Push((x, y));
+            int perimeter = 0;
+
+            while (stack.Count > 0)
+            {
+                var (cx, cy) = stack.Pop();
+                if (visited[cx, cy])
+                    continue;
+
+                visited[cx, cy] = true;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nx = cx + directions[i, 0];
+                    int ny = cy + directions[i, 1];
+
+                    if (!IsValid(nx, ny) || grid[nx][ny] != letter)
+                    {
+                        // Edge of the grid or different letter contributes to the perimeter
+                        perimeter++;
+                    }
+                    else if (!visited[nx, ny])
+                    {
+                        stack.Push((nx, ny));
+                    }
+                }
+            }
+
+            return perimeter;
+        }
+
+        int totalPerimeter = 0;
+
+        // Iterate over the grid to find all regions of the given letter
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (grid[i][j] == letter && !visited[i, j])
+                {
+                    totalPerimeter += FloodFillPerimeter(i, j);
+                }
+            }
+        }
+
+        return totalPerimeter;
+    }
+
+    public static int Calculate(List<List<char>> input)
+    {
+        var result = 0;
+        var flowers = GetUniqueCharacter(input);
+
+        foreach (var flower in flowers)
+        {
+            var area = GetArea(flower, input);
+            var perimeter = GetPerimeter(flower, input);
+            result += area * perimeter;
+        }
+
+        return result;
+    }
+
+    public static int CalculateTotalPrice(List<List<char>> map)
+    {
+        int rows = map.Count;
+        int cols = map[0].Count;
+        bool[,] visited = new bool[rows, cols];
+        int totalPrice = 0;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                if (!visited[r, c])
+                {
+                    (int area, int perimeter) = FloodFill(map, visited, r, c, map[r][c]);
+                    totalPrice += area * perimeter;
+                }
+            }
+        }
+
+        return totalPrice;
+    }
+
+    static (int, int) FloodFill(List<List<char>> map, bool[,] visited, int startRow, int startCol, char plantType)
+    {
+        int rows = map.Count;
+        int cols = map[0].Count;
+        int area = 0;
+        int perimeter = 0;
+
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+        queue.Enqueue((startRow, startCol));
+        visited[startRow, startCol] = true;
+
+        int[] dRow = { -1, 1, 0, 0 };
+        int[] dCol = { 0, 0, -1, 1 };
+
+        while (queue.Count > 0)
+        {
+            var (currentRow, currentCol) = queue.Dequeue();
+            area++;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int newRow = currentRow + dRow[i];
+                int newCol = currentCol + dCol[i];
+
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols)
+                {
+                    if (map[newRow][newCol] == plantType && !visited[newRow, newCol])
+                    {
+                        queue.Enqueue((newRow, newCol));
+                        visited[newRow, newCol] = true;
+                    }
+                    else if (map[newRow][newCol] != plantType)
+                    {
+                        perimeter++;
+                    }
+                }
+                else
+                {
+                    perimeter++;
+                }
+            }
+        }
+
+        return (area, perimeter);
     }
 }
--- advent-of-code-2023/2024/Day12/Day12.Test/Tests.cs
+++ advent-of-code-2023/2024/Day12/Day12.Test/Tests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Day12.Src;
 using FluentAssertions;
 
@@ -6,20 +7,99 @@ namespace Day12.Test
     public class Tests
     {
         private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day12.Src/testData.txt";
+        private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day12.Src/testData1.txt";
+        private readonly string _realData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day12.Src/realData.txt";
 
 
         [Fact]
         public void ReadFile()
         {
             // Arrange
-            var expected = 5;
+            var expected = new List<List<char>>
+            {
+                new List<char> { 'A', 'A', 'A', 'A' },
+                new List<char> { 'B', 'B', 'C', 'D' },
+                new List<char> { 'B', 'B', 'C', 'C' },
+                new List<char> { 'E', 'E', 'E', 'C' },
+
+            };
 
             // Act
             var result = CodeSolution.ReadFile(_testData);
 
             // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void GetUniqueCharacter()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+            var expected = new HashSet<char> { 'A', 'B', 'C', 'D', 'E'};
+
+            // Act
+            var result = CodeSolution.GetUniqueCharacter(data);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void GetArea()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.GetArea('A', data);
+
+            // Assert
+            result.Should().Be(4);
+        }
+
+        [Theory]
+        [InlineData('A', 10)]
+        [InlineData('B', 8)]
+        [InlineData('C', 10)]
+        [InlineData('D', 4)]
+        [InlineData('E', 8)]
+        public void GetPerimeter(char flower, int expected)
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.GetPerimeter(flower, data);
+
+            // Assert
             result.Should().Be(expected);
-            //result.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public void Calculate()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+
+            // Act
+            var result = CodeSolution.CalculateTotalPrice(data);
+
+            // Assert
+            result.Should().Be(140);
+        }
+
+        [Fact]
+        public void CalculateTD1()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_realData);
+
+            // Act
+            var result = CodeSolution.CalculateTotalPrice(data);
+
+            // Assert
+            result.Should().Be(1930);
         }
     }
 }
