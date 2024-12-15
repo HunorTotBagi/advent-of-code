aa5a20898e6a37c7d7e9c8e0aee56554fa452a0d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-15 21:33:39 +0200

    🎄2024 Day12: `Garden Groups` (#21)
    * feat: create initial setup
    
    * feat: unfinished part 1
    
    * chore: fix folders
    
    * chore: in progress
    
    * chore: fix

---
+++ advent-of-code-2023/2024/Day12/Day12.Test/Tests.cs
@@ -0,0 +1,52 @@
+using Day12.Src;
+using FluentAssertions;
+
+namespace Day12.Test
+{
+    public class Tests
+    {
+        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day12.Src/testData.txt";
+        private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day12.Src/testData1.txt";
+
+        [Fact]
+        public void ReadFile()
+        {
+            // Arrange
+            var expected = new List<List<char>>
+            {
+                new() { 'A', 'A', 'A', 'A' },
+                new() { 'B', 'B', 'C', 'D' },
+                new() { 'B', 'B', 'C', 'C' },
+                new() { 'E', 'E', 'E', 'C' },
+
+            };
+
+            // Act
+            var result = CodeSolution.ReadFile(_testData);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void GetRegions()
+        {
+            // Arrange
+            var data = CodeSolution.ReadFile(_testData);
+            var expected = new List<List<char>>
+            {
+                new() { 'A', 'A', 'A', 'A' },
+                new() { 'B', 'B', 'C', 'D' },
+                new() { 'B', 'B', 'C', 'C' },
+                new() { 'E', 'E', 'E', 'C' },
+
+            };
+
+            // Act
+            var result = CodeSolution.FloodFill(data);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
