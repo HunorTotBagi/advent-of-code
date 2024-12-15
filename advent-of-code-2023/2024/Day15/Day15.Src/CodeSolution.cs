4bd4f7e579518b471fb5a7742bc2d1aacc217c60
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-15 08:22:59 +0100

    feat: solve part 1

--- advent-of-code-2023/2024/Day15/Day15.Src/CodeSolution.cs
+++ advent-of-code-2023/2024/Day15/Day15.Src/CodeSolution.cs
@@ -2,17 +2,274 @@
 
 public class CodeSolution
 {
-    public static int ReadFile(string filePath)
+    public static List<List<char>> ReadFile(string filePath)
     {
         var lines = File.ReadAllLines(filePath);
 
-        var result1 = new List<List<int>>();
         var result2 = new List<List<char>>();
 
         foreach (var line in lines)
         {
-
+            var holder = new List<char>();
+            foreach (var c in line)
+            {
+                holder.Add(c);
+            }
+            result2.Add(holder);
         }
-        return 5;
+        return result2;
+    }
+
+    public static List<char> ReadMoves(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+
+        var result2 = new List<char>();
+
+        foreach (var line in lines)
+        {
+            foreach (var c in line)
+            {
+                result2.Add(c);
+            }
+        }
+
+        return result2;
+    }
+
+    public static (int x, int y) FindAtSign(List<List<char>> map)
+    {
+        for (var i = 0; i < map.Count; i++)
+        {
+            for (var j = 0; j < map[0].Count; j++)
+            {
+                if (map[i][j] == '@')
+                {
+                    return (i, j);
+                }
+            }
+        }
+
+        return (-1,-1);
+    }
+
+    public static int CountStonesInThatDirection(List<List<char>> map, char move, int x, int y)
+    {
+        var counter = 0;
+        if (move == '>')
+        {
+            while (map[x][y+1] == 'O')
+            {
+                counter++;
+                y++;
+            }
+        }
+
+        if (move == '<')
+        {
+            while (map[x][y-1] == 'O')
+            {
+                counter++;
+                y--;
+            }
+        }
+
+        if (move == 'v')
+        {
+            while (map[x+1][y] == 'O')
+            {
+                counter++;
+                x++;
+            }
+        }
+
+        if (move == '^')
+        {
+            while (map[x-1][y] == 'O')
+            {
+                counter++;
+                x--;
+            }
+        }
+        return counter;
+    }
+
+    public static List<List<char>> MoveStonesByOnePlace(List<List<char>> map, int x, int y, int numberOfStones, char direction)
+    {
+        if (direction == '<')
+        {
+            for (var k = y - numberOfStones - 1; k < y; k++)
+            {
+                map[x][k] = map[x][k + 1];
+                map[x][k + 1] = '.';
+            }
+        }
+
+        if (direction == '>')
+        {
+            for (var k = y + numberOfStones + 1; k > y; k--)
+            {
+                map[x][k] = map[x][k - 1];
+                map[x][k - 1] = '.';
+            }
+        }
+
+        if (direction == 'v')
+        {
+            for (var k = x + numberOfStones + 1; k > x; k--)
+            {
+                map[k][y] = map[k-1][y];
+                map[k-1][y] = '.';
+            }
+        }
+
+        if (direction == '^')
+        {
+            for (var k = x - numberOfStones - 1; k < x; k++)
+            {
+                map[k][y] = map[k + 1][y];
+                map[k + 1][y] = '.';
+            }
+        }
+
+        return map;
+    }
+
+    public static List<List<char>> ApplyAllMoves(List<List<char>> map, List<char> moves)
+    {
+        var (x,y)= FindAtSign(map);
+        foreach (var move in moves)
+        {
+            if (move == '<')
+            {
+                if (map[x][y-1] == '#')
+                    continue;
+
+                if (map[x][y - 1] == '.')
+                {
+                    map[x][y - 1] = '@';
+                    map[x][y] = '.';
+                    y--;
+
+                    continue;
+                }
+                    
+
+                if (map[x][y - 1] == 'O')
+                {
+                    var numberOfStones = CountStonesInThatDirection(map, move, x, y);
+
+                    if (map[x][y - numberOfStones - 1] == '.')
+                    {
+                        map = MoveStonesByOnePlace(map, x, y, numberOfStones, move);
+                        y--;
+                        continue;
+                    }
+                }
+            }
+
+            if (move == '^')
+            {
+                if (map[x-1][y] == '#')
+                    continue;
+
+                if (map[x - 1][y] == '.')
+                {
+                    map[x - 1][y] = '@';
+                    map[x][y] = '.';
+                    x--;
+                    continue;
+                }
+                    
+
+                if (map[x - 1][y] == 'O')
+                {
+                    var numberOfStones = CountStonesInThatDirection(map, move, x, y);
+
+                    if (map[x - numberOfStones - 1][y] == '.')
+                    {
+                        map = MoveStonesByOnePlace(map, x, y, numberOfStones, move);
+                        x--;
+                        continue;
+                    }
+                        
+                }
+            }
+
+            if (move == '>')
+            {
+                if (map[x][y + 1] == '#')
+                {
+                    continue;
+                }
+
+                if (map[x][y + 1] == '.')
+                {
+                    map[x][y + 1] = '@';
+                    map[x][y] = '.';
+                    y++;
+
+                    continue;
+                }
+                
+                if (map[x][y + 1] == 'O')
+                {
+                    var numberOfStones = CountStonesInThatDirection(map, move, x, y);
+
+                    if (map[x][y + numberOfStones + 1] == '.')
+                    {
+                        map = MoveStonesByOnePlace(map, x, y, numberOfStones, move);
+                        y++;
+                        continue;   
+                    }
+                }
+            }
+
+            if (move == 'v')
+            {
+                if (map[x+1][y] == '#')
+                    continue;
+
+                if (map[x + 1][y] == '.')
+                {
+                    map[x + 1][y] = '@';
+                    map[x][y] = '.';
+                    x++;
+
+                    continue;
+                }
+                
+                if (map[x + 1][y] == 'O')
+                {
+                    var numberOfStones = CountStonesInThatDirection(map, move, x, y);
+
+                    if (map[x + numberOfStones + 1][y] == '.')
+                    {
+                        map = MoveStonesByOnePlace(map, x, y, numberOfStones, move);
+                        x++;
+                    }
+                }
+            }
+        }
+
+        return map;
+    }
+
+    public static int CalculateGPS(List<List<char>> map)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < map.Count; i++)
+        {
+            for (int j = 0; j < map[i].Count; j++)
+            {
+                if (map[i][j] == 'O')
+                {
+                    sum += i * 100 + j;
+                }
+            }
+        }
+
+        return sum;
     }
 }
--- advent-of-code-2023/2024/Day15/Day15.Test/Tests.cs
+++ advent-of-code-2023/2024/Day15/Day15.Test/Tests.cs
@@ -5,7 +5,18 @@ namespace Day15.Test
 {
     public class Tests
     {
-        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData.txt";
+        private readonly string _testData0 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData0.txt";
+        private readonly string _testData1 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData1.txt";
+        private readonly string _testData2 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData2.txt";
+        private readonly string _testData3 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData3.txt";
+        private readonly string _testData4 = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData4.txt";
+        private readonly string _testDataLargeMap = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testDataLargeMap.txt";
+        private readonly string _testDataLargeMoves = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testDataLargeMoves.txt";
+        private readonly string _testDataLargeResult = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testDataLargeResult.txt";
+        private readonly string _testData5final = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testData5final.txt";
+
+        private readonly string _testDataLargeMapReal = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testDataLargeMapReal.txt";
+        private readonly string _testDataLargeMovesReal = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day15.Src/testDataLargeMovesReal.txt";
 
         [Fact]
         public void ReadFile()
@@ -14,11 +25,132 @@ namespace Day15.Test
             var expected = 5;
 
             // Act
-            var result = CodeSolution.ReadFile(_testData);
+            var result = CodeSolution.ReadFile(_testData0);
+            var moves = CodeSolution.ReadMoves(_testData1);
+
+            var a = 1;
+            // Assert
+            //result.Should().Be(expected);
+            //result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void CountStonesData2()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testData2);
+            var moves = CodeSolution.ReadMoves(_testData1);
+
+            // Act
+            var result = CodeSolution.CountStonesInThatDirection(map, '>', 1, 3);
 
             // Assert
-            result.Should().Be(expected);
-            //result.Should().BeEquivalentTo(expected);
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void CountStonesData3()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testData3);
+
+            // Act
+            var result = CodeSolution.CountStonesInThatDirection(map, 'v', 1, 4);
+
+            // Assert
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void MoveStonesData2()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testData2);
+            var numberOfStones = CodeSolution.CountStonesInThatDirection(map, '>', 1, 3);
+            var expected = CodeSolution.ReadFile(_testData3);
+
+            // Act
+            var result = CodeSolution.MoveStonesByOnePlace(map, 1, 3, numberOfStones, '>');
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void MoveStonesData3()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testData3);
+            var numberOfStones = CodeSolution.CountStonesInThatDirection(map, 'v', 1, 4);
+            var expected = CodeSolution.ReadFile(_testData4);
+
+            // Act
+            var result = CodeSolution.MoveStonesByOnePlace(map, 1, 4, numberOfStones, 'v');
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ApplyAllOnSmallData()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testData0);
+            var moves = CodeSolution.ReadMoves(_testData1);
+            var expected = CodeSolution.ReadFile(_testData5final);
+
+            // Act
+            var result = CodeSolution.ApplyAllMoves(map, moves);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void ApplyAllOnLargeData()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testDataLargeMap);
+            var moves = CodeSolution.ReadMoves(_testDataLargeMoves);
+            var expected = CodeSolution.ReadFile(_testDataLargeResult);
+
+            // Act
+            var result = CodeSolution.ApplyAllMoves(map, moves);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void CalcGPS()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testDataLargeMap);
+            var moves = CodeSolution.ReadMoves(_testDataLargeMoves);
+
+            var expected = CodeSolution.ApplyAllMoves(map, moves);
+
+            // Act
+            var result = CodeSolution.CalculateGPS(expected);
+
+            // Assert
+            result.Should().Be(10092);
+        }
+
+        [Fact]
+        public void CalcGPSREAL()
+        {
+            // Arrange
+            var map = CodeSolution.ReadFile(_testDataLargeMapReal);
+            var moves = CodeSolution.ReadMoves(_testDataLargeMovesReal);
+
+            var expected = CodeSolution.ApplyAllMoves(map, moves);
+
+            // Act
+            var result = CodeSolution.CalculateGPS(expected);
+
+            // Assert
+            result.Should().Be(10092);
         }
     }
 }
