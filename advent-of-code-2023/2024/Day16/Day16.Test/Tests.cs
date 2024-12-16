1d1b7da708c99df9a35bed730015ae053bb8eb0c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-16 06:43:23 +0100

    feat: solve part 1 & 2

--- advent-of-code-2023/2024/Day16/Day16.Test/Tests.cs
+++ advent-of-code-2023/2024/Day16/Day16.Test/Tests.cs
@@ -5,21 +5,36 @@ namespace Day16.Test
 {
     public class Tests
     {
-        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day16.Src/testData.txt";
-
-        [Fact]
-        public void ReadFile()
+        [Theory]
+        [InlineData("testData0.txt", 7036)]
+        [InlineData("testData1.txt", 11048)]
+        public void CalculateScore(string fileName, int expectedScore)
         {
             // Arrange
-            var expected = new List<List<int>>();
+            var fullPath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day16.Src/" + fileName;
+            var data = CodeSolution.ReadFile(fullPath);
 
             // Act
-            var result = CodeSolution.ReadFile(_testData);
+            var result = CodeSolution.FindLowestScore(data);
 
             // Assert
-            result.Should().BeEquivalentTo(expected);
-            //result.Should().Be(expected);
+            result.Should().Be(expectedScore);
+        }
 
+        [Theory]
+        [InlineData("testData0.txt", 45)]
+        [InlineData("testData1.txt", 64)]
+        public void FindBestPathTiles(string fileName, int expectedScore)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../Day16.Src/" + fileName;
+            var data = CodeSolution.ReadFile(filePath);
+
+            // Act
+            var result = CodeSolution.FindBestPathTiles(data);
+
+            // Assert
+            result.Should().Be(expectedScore);
         }
     }
 }
