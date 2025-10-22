f5b85b0cbece6be936c190ad39f575a8ac7cabe7
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-22 07:11:20 +0200

    feat: solve part 1 & 2

---
+++ 2015/Src/Day01/SolutionP1.cs
@@ -0,0 +1,20 @@
+﻿namespace Src.Day01;
+
+public class SolutionP1
+{
+    public static int GetTheFloor(string input)
+    {
+        var currentFloor = 0;
+
+        foreach (var character in input)
+        {
+            if (character == '(')
+                currentFloor++;
+
+            if (character == ')')
+                currentFloor--;
+        }
+
+        return currentFloor;
+    }
+}
