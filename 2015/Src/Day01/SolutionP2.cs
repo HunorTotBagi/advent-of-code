494171749c58d87bf7c5c5ee48f509c5232766aa
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-22 08:15:08 +0300

    feat: solve part 1 & 2 (#28)

---
+++ 2015/Src/Day01/SolutionP2.cs
@@ -0,0 +1,33 @@
+﻿namespace Src.Day01;
+
+public class SolutionP2
+{
+    public static int GetBasementPositionIndex(string input)
+    {
+        var charArray = input.ToCharArray();
+        var currentFloor = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (charArray[i] == '(')
+            {
+                currentFloor++;
+                if (currentFloor == -1)
+                {
+                    return i + 1;
+                }
+            }
+
+            if (charArray[i] == ')')
+            {
+                currentFloor--;
+                if (currentFloor == -1)
+                {
+                    return i + 1;
+                }
+            }
+        }
+
+        return currentFloor;
+    }
+}
---
+++ 2015/Tests/Day01Tests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Src.Day01;
+using Xunit;
+
+namespace Tests;
+
+public class Day01Tests
+{
+    [Theory]
+    [InlineData("(())", 0)]
+    [InlineData("()()", 0)]
+    [InlineData("(((", 3)]
+    [InlineData("(()(()(", 3)]
+    [InlineData("))(((((", 3)]
+    [InlineData("())", -1)]
+    [InlineData("))(", -1)]
+    [InlineData(")))", -3)]
+    [InlineData(")())())", -3)]
+    [InlineData(")())())", -3)]
+    public void GetTheFloorTest(string input, int expected)
+    {
+        // Act
+        var result = SolutionP1.GetTheFloor(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(")", 1)]
+    [InlineData("()())", 5)]
+    public void GetBasementPositionIndexTest(string input, int expected)
+    {
+        // Act
+        var result = SolutionP2.GetBasementPositionIndex(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
