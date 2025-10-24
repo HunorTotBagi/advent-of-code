2e1937f3211ddf9df7c2ee1b464b31ebbbff7c4a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-24 12:50:40 +0200

    feat: solve part 1 & 2

---
+++ 2015/Src/Day03/SolutionP1.cs
@@ -0,0 +1,41 @@
+﻿namespace Src.Day03;
+
+public class SolutionP1
+{
+    public static int CalculateSantasWork(string s)
+    {
+        var currX = 0;
+        var currY = 0;
+        var seen = new HashSet<(int x, int y)> { (currX, currY) };
+
+        foreach (var c in s)
+        {
+            if (c == '^')
+            {
+                currY++;
+                seen.Add((currX, currY));
+            }
+
+            if (c == 'v')
+            {
+                currY--;
+                seen.Add((currX, currY));
+            }
+
+            if (c == '>')
+            {
+                currX++;
+                seen.Add((currX, currY));
+            }
+
+
+            if (c == '<')
+            {
+                currX--;
+                seen.Add((currX, currY));
+            }
+        }
+
+        return seen.Count;
+    }
+}
---
+++ 2015/Src/Day03/SolutionP2.cs
@@ -0,0 +1,77 @@
+﻿namespace Src.Day03;
+
+public class SolutionP2
+{
+    public static int CalculateSantasAndRobotsWork(string s)
+    {
+        var charArray = s.ToCharArray();
+
+        var santaX = 0;
+        var santaY = 0;
+
+        var robotX = 0;
+        var robotY = 0;
+
+        var seen = new HashSet<(int x, int y)> { (santaX, santaY), (robotX, robotY) };
+
+        for (var i = 0; i < charArray.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (charArray[i] == '^')
+                {
+                    santaY++;
+                    seen.Add((santaX, santaY));
+                }
+
+                if (charArray[i] == 'v')
+                {
+                    santaY--;
+                    seen.Add((santaX, santaY));
+                }
+
+                if (charArray[i] == '>')
+                {
+                    santaX++;
+                    seen.Add((santaX, santaY));
+                }
+
+
+                if (charArray[i] == '<')
+                {
+                    santaX--;
+                    seen.Add((santaX, santaY));
+                }
+            }
+            else
+            {
+                if (charArray[i] == '^')
+                {
+                    robotY++;
+                    seen.Add((robotX, robotY));
+                }
+
+                if (charArray[i] == 'v')
+                {
+                    robotY--;
+                    seen.Add((robotX, robotY));
+                }
+
+                if (charArray[i] == '>')
+                {
+                    robotX++;
+                    seen.Add((robotX, robotY));
+                }
+
+
+                if (charArray[i] == '<')
+                {
+                    robotX--;
+                    seen.Add((robotX, robotY));
+                }
+            }
+        }
+
+        return seen.Count;
+    }
+}
