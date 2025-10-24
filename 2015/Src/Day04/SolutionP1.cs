ba97c05a588f7be7a6c53ae0fd16049854fffbc2
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-24 19:01:43 +0200

    feat: solve 2015 day4 part 1 & 2

---
+++ 2015/Src/Day04/SolutionP1.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Src.Day04;
+
+public class SolutionP1
+{
+    public static int Find(string key, bool fiveZeroes)
+    {
+        using var md5 = MD5.Create();
+        var keyBytes = Encoding.ASCII.GetBytes(key);
+
+        for (var i = 1; ; i++)
+        {
+            var num = i.ToString();
+            var bytes = new byte[keyBytes.Length + num.Length];
+            Buffer.BlockCopy(keyBytes, 0, bytes, 0, keyBytes.Length);
+            Encoding.ASCII.GetBytes(num, 0, num.Length, bytes, keyBytes.Length);
+
+            var hash = md5.ComputeHash(bytes);
+
+            var ok = hash[0] == 0 && hash[1] == 0 && (fiveZeroes ? (hash[2] & 0xF0) == 0 : hash[2] == 0);
+
+            if (ok) return i;
+        }
+    }
+}
---
+++ 2015/Tests/Day04Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tests;
+using Src.Day04;
+
+public class Day04Tests
+{
+    [Theory]
+    [InlineData("abcdef", true, 609043)]
+    [InlineData("pqrstuv", true, 1048970)]
+    public void Test1(string secretKey, bool fiveZeros, int expected)
+    {
+        // Act
+        var result = SolutionP1.Find(secretKey, fiveZeros);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
