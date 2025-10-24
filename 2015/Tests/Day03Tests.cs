9b3fcd0d0fe47cf0f2a385f75334c00ecab3ca9b
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-24 13:56:35 +0300

    🎄2015 Day03: `Perfectly Spherical Houses in a Vacuum` (#30)
    * feat: solve part 1 & 2
    
    * chore: remove personal input data
    
    * refactor: utilize `else if` statement to improve the code

---
+++ 2015/Tests/Day03Tests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Src.Day03;
+using Xunit;
+
+namespace Tests;
+
+public class Day03Tests
+{
+    [Theory]
+    [InlineData(">", 2)]
+    [InlineData("^>v<", 4)]    
+    [InlineData("^v^v^v^v^v", 2)]    
+    public void Test1(string s, int expected)
+    {
+        // Act
+        var result = SolutionP1.CalculateSantasWork(s);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("^v", 3)]
+    [InlineData("^>v<", 3)]
+    [InlineData("^v^v^v^v^v", 11)]
+    public void Test2(string s, int expected)
+    {
+        // Act
+        var result = SolutionP2.CalculateSantasAndRobotsWork(s);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
