65727ffbb5c8df79ca89b0f957e941d2aa685099
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-10-25 08:08:04 +0200

    feat: solve 2015 day6 part 1 & 2

---
+++ 2015/Src/Day06/SolutionP2.cs
@@ -0,0 +1,69 @@
+﻿namespace Src.Day06;
+
+public class SolutionP2
+{
+    public static int Calculate(string filePath)
+    {
+        var lines = File.ReadAllLines(filePath);
+        var grid = new int[1000, 1000];
+
+        foreach (var line in lines)
+        {
+            var (command, cord1, cord2) = ExtractCommands(line);
+
+            if (command == "turn on")
+            {
+                for (var i = cord1[0]; i <= cord2[0];i++)
+                    for (var j = cord1[1]; j <= cord2[1]; j++)
+                        grid[i, j] += 1;
+            }
+
+            else if (command == "toggle")
+                for (var i = cord1[0]; i <= cord2[0]; i++)
+                    for (var j = cord1[1]; j <= cord2[1]; j++)
+                        grid[i, j] += 2;
+
+            else if (command == "turn off")
+                for (var i = cord1[0]; i <= cord2[0]; i++)
+                    for (var j = cord1[1]; j <= cord2[1]; j++)
+                    {
+                        if (grid[i, j] != 0)
+                            grid[i, j] -= 1;
+                    }
+        }
+
+        var count = 0;
+        for (var i = 0; i < 1000; i++)
+            for (var j = 0; j < 1000; j++)
+                count += grid[i, j];
+
+        return count;
+    }
+
+
+    public static (string command, int[] first, int[] second) ExtractCommands(string s)
+    {
+        var s1 = s.Split(" through ");
+        var secondCord = s1[1].Split(",");
+
+        var y1 = int.Parse(secondCord[0]);
+        var y2 = int.Parse(secondCord[1]);
+
+        var firstPart = s1[0].Split(",");
+        var zz = firstPart[0].Split(" ");
+
+        var command = "";
+        if (zz.Length == 3)
+            command = zz[0] + " " + zz[1];
+        else
+            command = zz[0];
+
+        var x1 = int.Parse(zz[^1]);
+        var x2 = int.Parse(firstPart[^1]);
+
+        var first = new[] { x1, x2 };
+        var second = new[] { y1, y2 };
+
+        return (command, first, second);
+    }
+}
---
+++ 2015/Tests/Day06Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Src.Day06;
+using Xunit;
+
+namespace Tests;
+
+public class Day06Tests
+{
+    [Theory]
+    [InlineData("turn on 0,0 through 999,999", "turn on", new[] {0,0}, new[] {999,999})]
+    [InlineData("toggle 0,0 through 999,0", "toggle", new[] {0,0}, new[] {999,0})]
+    [InlineData("turn off 499,499 through 500,500", "turn off", new[] {499,499}, new[] {500,500})]
+
+    public void Test(string input, string expectedCommand, int[] expected1, int[] expected2)
+    {
+        // Act
+        var result = SolutionP1.ExtractCommands(input);
+
+        // Assert
+        result.first.Should().BeEquivalentTo(expected1);
+        result.second.Should().BeEquivalentTo(expected2);
+        result.command.Should().Be(expectedCommand);
+    }
+}
