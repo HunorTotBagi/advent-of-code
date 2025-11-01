a8c11d8a99443f4036292fa02b16403f85b3c13e
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-11-01 12:59:02 +0100

    feat: solve 2015 day9 part 1

---
+++ 2015/Src/Day09/SolutionP1.cs
@@ -0,0 +1,83 @@
+﻿namespace Src.Day09;
+
+public class SolutionP1
+{
+    public static int Calc(string filePath)
+    {
+        var distances = new Dictionary<(string, string), int>();
+        var cities = new HashSet<string>();
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            var (from, to, d) = Split(line);
+            cities.Add(from);
+            cities.Add(to);
+            distances[Canonical(from, to)] = d;
+        }
+
+        var cityList = cities.ToList();
+        var best = int.MaxValue;
+
+        foreach (var perm in Permute(cityList))
+        {
+            var sum = 0;
+            var ok = true;
+
+            for (var i = 0; i < perm.Count - 1; i++)
+            {
+                if (!distances.TryGetValue(Canonical(perm[i], perm[i + 1]), out var w))
+                {
+                    ok = false; // missing edge (shouldn’t happen in this puzzle)
+                    break;
+                }
+
+                sum += w;
+
+                if (sum >= best)
+                {
+                    ok = false; // prune early
+                    break;
+                }
+            }
+
+            if (ok && sum < best)
+                best = sum;
+        }
+
+        return best;
+    }
+
+    public static (string from, string to, int distance) Split(string input)
+    {
+        var firstSplit = input.Split(" = ");
+        var secondSplit = firstSplit[0].Split(" to ");
+        return (secondSplit[0], secondSplit[1], int.Parse(firstSplit[1]));
+    }
+
+    public static (string, string) Canonical(string a, string b) =>
+        string.CompareOrdinal(a, b) <= 0 ? (a, b) : (b, a);
+
+    public static IEnumerable<List<string>> Permute(List<string> items)
+    {
+        var arr = items.ToArray();
+        foreach (var p in PermuteRec(arr, 0))
+            yield return p.ToList();
+    }
+
+    public static IEnumerable<string[]> PermuteRec(string[] arr, int start)
+    {
+        if (start == arr.Length)
+        {
+            yield return (string[])arr.Clone();
+            yield break;
+        }
+
+        for (var i = start; i < arr.Length; i++)
+        {
+            (arr[start], arr[i]) = (arr[i], arr[start]);
+            foreach (var p in PermuteRec(arr, start + 1))
+                yield return p;
+            (arr[start], arr[i]) = (arr[i], arr[start]);
+        }
+    }
+}
---
+++ 2015/Tests/Day09Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+using FluentAssertions;
+using Src.Day09;
+using Xunit;
+
+namespace Tests;
+
+public class Day09Tests
+{
+    private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Day09/testData.txt";
+
+    [Theory]
+    [InlineData("London to Dublin = 464", "London", "Dublin", 464)]
+    [InlineData("London to Belfast = 518", "London", "Belfast", 518)]
+    [InlineData("Dublin to Belfast = 141", "Dublin", "Belfast", 141)]
+    public void SplitTest(string input, string from, string to, int distance)
+    {
+        // Act
+        var result = SolutionP1.Split(input);
+
+        // Assert
+        result.Item1.Should().Be(from);
+        result.Item2.Should().Be(to);
+        result.Item3.Should().Be(distance);
+    }
+
+    [Theory]
+    [InlineData("Novi Sad", "Beograd",  "Beograd", "Novi Sad")]
+    [InlineData("Beograd", "Novi Sad", "Beograd", "Novi Sad")]
+    public void CanonicalTest(string from, string to, string expected1, string expected2)
+    {
+        // Act
+        var result = SolutionP1.Canonical(from, to);
+
+        // Assert
+        result.Item1.Should().Be(expected1);
+        result.Item2.Should().Be(expected2);
+    }
+
+    [Fact]
+    public void CalcTest()
+    {
+        var result = SolutionP1.Calc(_testData);
+
+        result.Should().Be(605);
+    }
+}
