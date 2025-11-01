d24697b7cc9012e333f0899bdda0b735713cab60
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-11-01 14:05:38 +0100

    feat: solve 2015 day10 part 1 & 2

---
+++ 2015/Src/Day10/SolutionP1.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Src.Day10;
+
+public class SolutionP1
+{
+    public static int Calc(string input)
+    {
+        var i = 0;
+        var result = input;
+        while (i < 40)
+        {
+            result = Transform(result);
+            i++;
+        }
+
+        return result.Length;
+    }
+    public static string Transform(string input)
+    {
+        var result = new StringBuilder(input.Length * 2);
+
+        var counter = 0;
+        var current = input[0];
+
+        var j = 0;
+        while (j < input.Length)
+        {
+            while (j < input.Length && input[j] == current)
+            {
+                counter++;
+                j++;
+            }
+
+            result.Append(counter);
+            result.Append(current);
+
+            if (j < input.Length)
+            {
+                current = input[j];
+                counter = 0;
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ 2015/Tests/Day10Tests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Src.Day10;
+using Xunit;
+
+namespace Tests;
+
+public class Day10Tests
+{
+    [Theory]
+    [InlineData("1", "11")]
+    [InlineData("11", "21")]
+    [InlineData("1211", "111221")]
+    [InlineData("111221", "312211")]
+    public void TransformTest(string input, string expected)
+    {
+        // Act
+        var result = SolutionP1.Transform(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
