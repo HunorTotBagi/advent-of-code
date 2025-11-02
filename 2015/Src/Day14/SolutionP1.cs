8ebcc07b9990fa377d55abdb04fdcfbf56a485cf
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-11-02 21:00:34 +0100

    feat: solve 2015 day14 part 1

---
+++ 2015/Src/Day14/SolutionP1.cs
@@ -0,0 +1,55 @@
+﻿namespace Src.Day14;
+
+public class SolutionP1
+{
+    public static int Calc(string filePath, int seconds)
+    {
+        var lines = File.ReadAllLines(filePath);
+
+        var speeds = new List<int>();
+        var activeTimes = new List<int>();
+        var restTimes = new List<int>();
+
+        foreach (var line in lines)
+        {
+            var (speed, activeTime, restTime) = ExtractInfo(line);
+
+            speeds.Add(speed);
+            activeTimes.Add(activeTime);
+            restTimes.Add(restTime);
+        }
+
+        var max = int.MinValue;
+
+        for (var i = 0; i < speeds.Count; i++)
+        {
+            var current = GetDistance(speeds[i], activeTimes[i], restTimes[i], seconds);
+            if (current > max)
+                max = current;
+        }
+
+        return max;
+    }
+
+    public static (int speed, int activeTime, int restTime) ExtractInfo(string input)
+    {
+        var a = input.Split(" seconds, but then must rest for ");
+        var b = a[1].Split(" ");
+        var c = a[0].Split(" ");
+
+        var speed = int.Parse(c[3]);
+        var activeTime = int.Parse(c[6]);
+        var restTime = int.Parse(b[0]);
+
+        return (speed, activeTime, restTime);
+    }
+
+    public static int GetDistance(int speed, int activeTime, int restTime, int targetSeconds)
+    {
+        var cycle = activeTime + restTime;
+        var cycles = targetSeconds / cycle;
+        var remainder = targetSeconds % cycle;
+
+        return cycles * speed * activeTime + speed * Math.Min(activeTime, remainder);
+    }
+}
---
+++ 2015/Tests/Day14Tests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Src.Day14;
+using Xunit;
+
+namespace Tests;
+
+public class Day14Tests
+{
+    private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Day14/testData.txt";
+
+    [Theory]
+    [InlineData("Vixen can fly 8 km/s for 8 seconds, but then must rest for 53 seconds.", 8, 8, 53)]
+    [InlineData("Rudolph can fly 20 km/s for 7 seconds, but then must rest for 132 seconds.", 20, 7, 132)]
+    public void Test1(string input, int expectedSpeed, int expectedActiveTime, int expectedRestTime)
+    {
+        // Act
+        var (speed, activeTime, restTime) = SolutionP1.ExtractInfo(input);
+
+        // Arrange
+        speed.Should().Be(expectedSpeed);
+        activeTime.Should().Be(expectedActiveTime);
+        restTime.Should().Be(expectedRestTime);
+    }
+
+    [Theory]
+    [InlineData(14,10,127, 1120)]
+    [InlineData(16,11,162, 1056)]
+    public void GetDistanceTest(int speed, int activeTime, int restTime, int expected)
+    {
+        // Act
+        var result = SolutionP1.GetDistance(speed, activeTime, restTime, 1000);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var seconds = 1000;
+
+        // Act
+        var result = SolutionP1.Calc(_testData, seconds);
+
+        // Assert
+        result.Should().Be(1120);
+    }
+}
