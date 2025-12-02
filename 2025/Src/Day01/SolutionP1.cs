d7a5cf3799713520069ef1e3cd1e284c718111b0
Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>
2025-12-02 20:09:45 +0100

    feat: solve 2025 day01 p1

---
+++ 2025/Src/Day01/SolutionP1.cs
@@ -0,0 +1,46 @@
+﻿namespace Src.Day01;
+
+public class SolutionP1
+{
+    public static int Solve(string filePath)
+    {
+        var instructions = ReadFile(filePath);
+
+        var position = 50;
+        var countAtZero = 0;
+
+        foreach (var (direction, distance) in instructions)
+        {
+            var steps = distance % 100;
+
+            if (direction == 'R')
+                position = (position + steps) % 100;
+
+            else if (direction == 'L')
+                position = (position - steps + 100) % 100;
+
+            if (position == 0)
+                countAtZero++;
+        }
+
+        return countAtZero;
+    }
+
+
+
+    public static List<(char, int)> ReadFile(string filePath)
+    {
+        var result = new List<(char, int)>();
+        var lines = File.ReadAllLines(filePath);
+
+        foreach (var line in lines)
+        {
+            var direction = line[0];
+            var distance = int.Parse(line[1..]);
+
+            result.Add((direction, distance));
+        }
+
+        return result;
+    }
+}
---
+++ 2025/Tests/Day01Tests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Src.Day01;
+
+namespace Tests
+{
+    public class Day01Tests
+    {
+        private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Day01/testData.txt";
+
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var expected = new List<(char, int)>
+            {
+                ('L', 68),
+                ('L', 30),
+                ('R', 48),
+                ('L', 5),
+                ('R', 60),
+                ('L', 55),
+                ('L', 1),
+                ('L', 99),
+                ('R', 14),
+                ('L', 82),
+            };
+
+            // Act
+            var result = SolutionP1.ReadFile(_testData);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var expected = 3;
+
+            // Act
+            var result = SolutionP1.Solve(_testData);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
