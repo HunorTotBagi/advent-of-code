c026f5b92152110764d6374722c61eecb2c5442d
Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>
2026-01-01 20:36:16 +0100

    feat: add `ReadFile` method

---
+++ 2025/Src/Day02/SolutionP1.cs
@@ -0,0 +1,13 @@
+﻿namespace Src.Day02;
+
+public class SolutionP1
+{
+    public static List<string> ReadFile(string filePath)
+    {
+        var line = File.ReadAllLines(filePath);
+
+        var separated = line[0].Split(',');
+
+        return separated.ToList();
+    }
+}
