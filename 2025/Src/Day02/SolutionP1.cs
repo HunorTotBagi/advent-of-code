d19d297f7eb2be93a8b10e86cf544afd16809d9f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2026-01-02 07:42:09 +0100

    🎄2025 Day02: `Gift Shop` (#31)
    * feat: add `ReadFile` method
    
    * feat: add `IsValidDigit` method
    
    * feat: add final `Solve` method
    
    * feat: utilize long as a variable to fit larger numbers
    
    * feat: complete p2 with adjusted check on valid numbers
    
    * chore: refactor test file
    
    * chore: refactor solution files
    
    ---------
    
    Co-authored-by: Hunor Tot-Bagi <hunor.totbagi@rationaletech.com>

---
+++ 2025/Src/Day02/SolutionP1.cs
@@ -0,0 +1,46 @@
+﻿namespace Src.Day02;
+
+public class SolutionP1
+{
+    public static long Solve(string filePath)
+    {
+        var input = ReadFile(filePath);
+        long result = 0;
+
+        foreach (var range in input)
+        {
+            var start = long.Parse(range.Split('-')[0]);
+            var end = long.Parse(range.Split('-')[1]);
+
+            for (var i=start; i<=end; i++)
+                if (IsValidDigit(i))
+                    result+=i;
+        }
+
+        return result;
+    }
+
+    public static bool IsValidDigit(long number)
+    {
+        var numberAsString = number.ToString();
+
+        if (numberAsString.Length %2 != 0)
+            return false;
+
+        var firstHalf = numberAsString[..(numberAsString.Length / 2)];
+        var secondHalf = numberAsString[(numberAsString.Length / 2)..];
+
+        if (firstHalf == secondHalf)
+            return true;
+
+        return false;
+    }
+
+    public static List<string> ReadFile(string filePath)
+    {
+        var line = File.ReadAllLines(filePath);
+        var separated = line[0].Split(',');
+
+        return separated.ToList();
+    }
+}
---
+++ 2025/Tests/Day02Tests.cs
@@ -0,0 +1,88 @@
+﻿using FluentAssertions;
+
+namespace Tests;
+
+public class Day02Tests
+{
+    private readonly string _testData = AppDomain.CurrentDomain.BaseDirectory + "../../../../Src/Day02/testData.txt";
+
+    [Fact]
+    public void ReadFileTest()
+    {
+        // Arrange
+        var expected = new List<string>
+        {
+            "11-22",
+            "95-115",
+            "998-1012",
+            "1188511880-1188511890",
+            "222220-222224",
+            "1698522-1698528",
+            "446443-446449",
+            "38593856-38593862",
+            "565653-565659",
+            "824824821-824824827",
+            "2121212118-2121212124"
+        };
+
+        // Act
+        var result = Src.Day02.SolutionP1.ReadFile(_testData);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Theory]
+    [InlineData(12341234, true)]
+    [InlineData(11, true)]
+    [InlineData(123, false)]
+    [InlineData(351159, false)]
+    public void IsValidDigitTest(int number, bool expected)
+    {
+        // Act
+        var result = Src.Day02.SolutionP1.IsValidDigit(number);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void SolveP1Test()
+    {
+        // Arrange
+        var expected = 1227775554;
+
+        // Act
+        var result = Src.Day02.SolutionP1.Solve(_testData);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(12341234, true)]
+    [InlineData(11, true)]
+    [InlineData(123, false)]
+    [InlineData(351159, false)]
+    public void IsValidDigitAdvancedTest(int number, bool expected)
+    {
+        // Act
+        var result = Src.Day02.SolutionP2.IsValidDigit(number);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void SolveP2Test()
+    {
+        // Arrange
+        var expecetd = 4174379265;
+
+        // Act
+        var result = Src.Day02.SolutionP2.Solve(_testData);
+
+        // Assert
+        result.Should().Be(expecetd);
+    }
+}
